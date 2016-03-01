0bf3330b0745011442d734b7f95c027f94d616e4
Ran <ran.guin@gmail.com>
2016-02-29 21:22:54 -0800

    fix header

---
+++ assets/js/dependencies/custom.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+    
+    setTimeout(function sunrise () {
+	// *** final color for header animation *** //
+          document.getElementsByClassName('header')[0].style.backgroundColor = '#832';
+    }, 0);  
+    
+    $('#searchResults').DataTable();
+
+
+} );
+
