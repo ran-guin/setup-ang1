fc46addf646d8967a51df94d6988668c67bb4cfc
Ran <ran.guin@gmail.com>
2016-02-29 20:57:35 -0800

    update transfer logic

--- api/controllers/ContainerController.js
+++ api/controllers/ContainerController.js
@@ -10,7 +10,7 @@ var bodyParser = require('body-parser');
 module.exports = {
 	
 	transfer : function (req, res ) {
-		req.body = { size : 1, target_size: '3x6', target_format : 5, prep_id : 7}; // test
+		req.body = { size : 1, target_size: '3x6', target_format : 5, prep_id : 7, target_rows: ['A','B','C','D'], target_cols : [1,2,3,4,5,6] }; // test
 		
 		var id = req.body.id || '';
 		var ids = id.split(/\s*,\s*/);
@@ -19,10 +19,12 @@ module.exports = {
 		var target_size   = req.body.target_size || 1;
 		var target_format = req.body.target_format;
 		var prep_id = req.body.prep_id;
-
+		var target_cols = req.body.target_cols;
+		var target_rows = req.body.target_rows;
+		// TEST 
 		ids = [];
-		for (var i=3; i<=303; i++) {
-			ids.push(i);
+		for (var i=1; i<=300; i++) {
+			ids.push(i )+ 300;
 		} // test
 
 		if (size == target_size) {
@@ -37,12 +39,20 @@ module.exports = {
 			});
 		}
 		else {
-			var params = { id: ids, target_format_id: target_format, prep_id : prep_id, target_size: target_size}; // TEST
+			var params = { id: ids, target_format_id: target_format, prep_id : prep_id, target_size: target_size, target_cols : target_cols, target_rows : target_rows}; // TEST
 			// Track transfer as rearray (track individual well movement)
+
+			console.log('test rearray transfer');
 			Container.rearray_transfer( params )
-			.then ( function (barcodes) {
+			.then ( function (data) {
 //				Barcode.generate(barcodes);
-				return res.send('Rearray Success: ' + JSON.stringify(barcodes)); 
+				var map = data.map;
+				var rows = data.rows;
+				var cols = data.cols;
+				var targets = data.targets;
+
+				console.log("rows: " + JSON.stringify(rows));
+				return res.render('lims/WellMap', { Map : map, rows: rows, cols: cols, targets : targets}); 
 			})
 			.catch ( function (err) {
 				return res.send('Rearray Error');
@@ -51,4 +61,4 @@ module.exports = {
 	}
 };
 
- 
+  
