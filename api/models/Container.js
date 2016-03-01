2d88be66f90e734cdc22e03d9513a74fc833ed7f
Ran <ran.guin@gmail.com>
2016-02-29 20:58:50 -0800

    update transfer logic

--- api/models/Container.js
+++ api/models/Container.js
@@ -5,6 +5,8 @@
 * @docs        :: http://sailsjs.org/#!documentation/models
 */
 
+var q = require('q');
+
 module.exports = {
 
 	attributes: {
@@ -30,20 +32,25 @@ module.exports = {
 
 	rearray_transfer : function ( transfer_parameters ) {
 
-		var ids = transfer_parameters['id'];
+		// input parameters: id [], target_format_id, prep_id, target_size, target_rows, target_cols
 
-		var targets = [];
-		for (var i=301; i<=401; i++) {
-			targets.push(i);
-		} // test
+		var ids = transfer_parameters['id'];
+		var deferred = q.defer();
 
 		var target_format_id = transfer_parameters['target_format_id'];
 		var prep_id = transfer_parameters['prep_id'];
 		var target_size = transfer_parameters['target_size'];
 
 		var target_dimensions = target_size.split('\s*x\s*');  // 8 x 12 -> [8,12]
-		var x_size = 1;
-		var y_size = 1;
+
+		var rows = transfer_parameters['target_rows'] || ['A'];
+		var cols = transfer_parameters['target_cols'] || [1];
+
+		var x_min = rows[0];
+		var x_max = rows[rows.length-1];
+		var y_min = cols[0];
+		var y_max = cols[cols.length-1];
+
 		if (target_dimensions.length>1) {
 			x_size = target_dimensions[0];
 			y_size = target_dimensions[1];
@@ -51,40 +58,77 @@ module.exports = {
 
 		var fill_by = 'column';  //  add row option later...
 
-		var x = 'A';  
+		var x = x_min;  
 		var y = 1;  
 
-		var target_index = 0;
 		var array = [];
 		var rearray = [];
+		var preserve_x = 8;
+
+		var map = {};
+
+		var target_index = 0;
+		var target_position = x_min + y_min.toString();
+		var targets = ['PLA0']; // CUSTOM TEST
+
+		array.push(targets[target_index], target_position);  // store mul plate record... 
+		rearray.push([ids[i], Container.position(ids[i]), targets[target_index], target_position]);
+
 		for (var i=0; i<ids.length; i++) {
 
-			// if (fill_by == 'column') {
+			if (fill_by == 'row') {
 				y++;
-				if (y > y_size) {
+				if (y > y_max) {
 					y = 1;
-					x++;
-
-					if (x > x_size) {
-						x='A';
-						y = 1;
+										
+					if (x == x_max) {
+						x=x_min;
+						y = y_min;
 						target_index++;       // next plate ... 
 					}
+					else {
+						x = String.fromCharCode(x.charCodeAt(0) + 1);
+					}
 				}
-			// }
-			var target_position = x + y;
+			}
+			else {
+				if (x == x_max) {
+					x=x_min;
+					if (y >= y_max) {
+						y=y_min;
+						target_index++;       // next plate ... 
+						targets.push('PLA' + target_index );
+					}
+					else {
+						y++;
+					}
+				}	
+				else {
+					x = String.fromCharCode(x.charCodeAt(0) + 1);
+				}
+
+			}
+
+			var target_position = x + y.toString();
+			console.log(target_position);
 			
 			array.push(targets[target_index], target_position);  // store mul plate record... 
 
-			rearray.push([ids[i], Container.position(ids[i]), targets[target_index], x+y]);
+			var index = targets[target_index] + ':' + target_position;
+			map[index] = ids[i];
+
+			rearray.push([ids[i], Container.position(ids[i]), targets[target_index], target_position]);
 		}
-		console.log("Transfer Map: " + JSON.stringify(rearray));
-		console.log("Multiplex Plates: " + JSON.stringify(array))
-		return array;
+		console.log("Map to " + JSON.stringify(targets));
+		var data = { map : map, targets : targets, rows : rows,  cols : cols };
+		console.log("xfer data: " + JSON.stringify(data));
+
+		deferred.resolve(data);
+		return deferred.promise;
 	},
 
 	position : function (id) {
-		return ' (position of ' + id;
+		return ' i' + id;
 	},
 
 	custom_transfer : function ( custom_parameters ) {
@@ -99,4 +143,4 @@ module.exports = {
 
 };
 
- 
+  
