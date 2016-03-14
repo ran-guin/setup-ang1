16a412cc0eecbb5f4ea7cc631fc445c86c832eab
Ran Guin <rguin@Rans-iMac.local>
2016-03-14 11:12:14 -0700

    tweak colour mapper & add static colour map and test

--- assets/js/dependencies/WellMapper.js
+++ assets/js/dependencies/WellMapper.js
@@ -34,6 +34,8 @@ function wellMapper() {
     //
     //  Returns attributes: 
     //   targetMap : hash (kys = colours;  values = array of rgb values corresponding to gradually lighter shades of colour) 
+    //
+    // Use for testing .. but Speed up in production by only including static map loading ... 
 
     this.colours = [];
     this.rgbList = [];
@@ -47,17 +49,18 @@ function wellMapper() {
 
         var list = [];
         
-        var colourList = ['red','purple','blue','teal','green','yellow','organge','brown', 'grey'];
+        var colourList = ['red','purple','blue','teal','green','yellow','orange','grey'];
         var hex = ['00','1a','33','4d','66','80','99','b3','cc','e6','ff'];  // domain of rgb values to use 
 
         // define array of starting points for rgb values (eg N -> hex[N] for each colour)
-        var reds = [7, 5, 0, 0, 0, 7, 6, 2, 1];
-        var greens = [0, 0, 0, 5, 4, 7, 1, 1, 1];
-        var blues = [0, 5, 5, 5, 0, 0, 2, 0, 1];
+        var reds   = [7, 5, 0, 0, 0, 5, 6, 3, 1];
+        var greens = [0, 0, 0, 5, 5, 5, 4, 2, 1];
+        var blues  = [0, 5, 5, 5, 0, 0, 1, 1, 1];
 
         var Map = {};    
-        var rgbList = [];
+
         var colours = [];
+        var rgbList = [];
 
         var maxIndex = hex.length - 1;
 
@@ -69,9 +72,10 @@ function wellMapper() {
             var green = greens[c];
             var blue = blues[c];
 
-            for (var i=1; i<=shades; i++) {
+            for (var i=0; i<shades; i++) {
 
-               if (red >= maxIndex) { 
+                var rgb = hex[red] + hex[green] + hex[blue];
+                if (red >= maxIndex) { 
 
                     if (blue < red) { blue++ }
                     if (green < red) { green++ }
@@ -94,36 +98,64 @@ function wellMapper() {
                     blue = maxIndex;
                     green++;
                     red++;
-                }           
-                else if (red) {
+                }    
+                else if (red == green && red == blue ) {
+                    red++;
+                    green++;
+                    blue++;
+                }       
+                else if (red >= green && red >= blue) {
                     if (green == red) { green++ }
                     if (blue == red) { blue++ }
                     red++;
                 }
-                else if (green) {
+                else if (green >= red && green >=blue) {
                     if (red == green) { red++ }
                     if (blue == green) { blue++ }
                     green++
                 }
-                else if (blue) {
+                else if (blue >= red && blue >= green) {
                     if (red == blue) { red++ }
                     if (green == blue) { green++ }
                     blue++
                 }       
 
-                var rgb = hex[red] + hex[green] + hex[blue];
 
                 Map[colours[c]].push(rgb);
                 rgbList.push(rgb);
+
             }
         }
-        
+
+        var rows = ['A','B','C','D','E','F','G','H'];
+        var columns = 12; 
+        var CMap = {};
+        for (var ri=0; ri < rows.length; ri++) {
+            for (var ci=1; ci <= columns; ci++) {
+                CMap[rows[ri] + ci.toString()] = Map[colours[ri]][ci-1];
+            }
+        }
+
+        this.CMap = CMap;
+        console.log("CMAP: " + JSON.stringify(CMap));
+
         this.colours = colours;
         this.rgbList = rgbList;
+
         this.Map = Map;
  
         return { Map : Map, list : rgbList, colours : colours }
     },
+
+    this.staticMap = function() {
+
+        this.colours = ['red','purple','blue','teal','green','yellow','orange','grey'];
+
+        this.ColourMap = {"A1":"b30000","A2":"cc0000","A3":"e60000","A4":"ff0000","A5":"ff1a1a","A6":"ff3333","A7":"ff4d4d","A8":"ff6666","A9":"ff8080","A10":"ff9999","A11":"ffb3b3","A12":"ffcccc","B1":"800080","B2":"990099","B3":"b300b3","B4":"cc00cc","B5":"e600e6","B6":"ff00ff","B7":"ff1aff","B8":"ff33ff","B9":"ff4dff","B10":"ff66ff","B11":"ff80ff","B12":"ff99ff","C1":"000080","C2":"000099","C3":"0000b3","C4":"0000cc","C5":"0000e6","C6":"0000ff","C7":"1a1aff","C8":"3333ff","C9":"4d4dff","C10":"6666ff","C11":"8080ff","C12":"9999ff","D1":"008080","D2":"009999","D3":"00b3b3","D4":"00cccc","D5":"00e6e6","D6":"00ffff","D7":"1affff","D8":"33ffff","D9":"4dffff","D10":"66ffff","D11":"80ffff","D12":"99ffff","E1":"008000","E2":"009900","E3":"00b300","E4":"00cc00","E5":"00e600","E6":"00ff00","E7":"1aff1a","E8":"33ff33","E9":"4dff4d","E10":"66ff66","E11":"80ff80","E12":"99ff99","F1":"808000","F2":"999900","F3":"b3b300","F4":"cccc00","F5":"e6e600","F6":"ffff00","F7":"ffff1a","F8":"ffff33","F9":"ffff4d","F10":"ffff66","F11":"ffff80","F12":"ffff99","G1":"99661a","G2":"b3661a","G3":"cc661a","G4":"e6661a","G5":"ff661a","G6":"ff8033","G7":"ff994d","G8":"ffb366","G9":"ffcc80","G10":"ffe699","G11":"ffffb3","G12":"ffffcc","H1":"4d331a","H2":"66331a","H3":"80331a","H4":"99331a","H5":"b3331a","H6":"cc331a","H7":"e6331a","H8":"ff331a","H9":"ff4d33","H10":"ff664d","H11":"ff8066","H12":"ff9980"};
+
+        this.rgbList = ["b30000","cc0000","e60000","ff0000","ff1a1a","ff3333","ff4d4d","ff6666","ff8080","ff9999","ffb3b3","ffcccc","800080","990099","b300b3","cc00cc","e600e6","ff00ff","ff1aff","ff33ff","ff4dff","ff66ff","ff80ff","ff99ff","000080","000099","0000b3","0000cc","0000e6","0000ff","1a1aff","3333ff","4d4dff","6666ff","8080ff","9999ff","008080","009999","00b3b3","00cccc","00e6e6","00ffff","1affff","33ffff","4dffff","66ffff","80ffff","99ffff","008000","009900","00b300","00cc00","00e600","00ff00","1aff1a","33ff33","4dff4d","66ff66","80ff80","99ff99","808000","999900","b3b300","cccc00","e6e600","ffff00","ffff1a","ffff33","ffff4d","ffff66","ffff80","ffff99","99661a","b3661a","cc661a","e6661a","ff661a","ff8033","ff994d","ffb366","ffcc80","ffe699","ffffb3","ffffcc","4d331a","66331a","80331a","99331a","b3331a","cc331a","e6331a","ff331a","ff4d33","ff664d","ff8066","ff9980"];
+
+    }
 
     this.col = function () {
 
--- assets/js/private/protocol/WellController.js
+++ assets/js/private/protocol/WellController.js
@@ -20,6 +20,8 @@ function wellController ($scope, $rootScope, $http, $q ) {
         if (config) { Config = JSON.parse(config) }
 
         $scope.sources = Config['sources'] || [];
+        $scope.targets = [];
+        $scope.source_init = $scope.sources;
         $scope.target  = Config['target'] || {};
         $scope.options = Config['options'];
 
@@ -53,20 +55,64 @@ function wellController ($scope, $rootScope, $http, $q ) {
         $scope.colourMap = newMap.Map;
         $scope.colours   = newMap.colours;
         $scope.rgbList = newMap.rgbList;
-
+ 
         console.log("cols: " + JSON.stringify(newMap.colours));
 
         console.log("MAP: " + JSON.stringify($scope.colourMap));
         console.log("colours: " + JSON.stringify($scope.colours));
         console.log("rgb: " + JSON.stringify($scope.rgbList));
-
         console.log("Source Map: " + JSON.stringify(newMap.distribute()));
+        console.log("NEW CMAP: " + JSON.stringify(newMap.CMap));
 
     }
 
+    $scope.source_by_Col = function source_by_Col () {
+        $scope.byCol = true;
+        $scope.byRow = false;
+        //$scope.sources = _.sortByNat($scope.sources, 'position');
+        $scope.sources = _.sortByNat($scope.sources, function(sample) { 
+            var string = sample.container.toString() + '_' + sample.position;
+            return string;
+        });
+    }
+
+    $scope.source_by_Row = function source_by_Row () {
+        $scope.byCol = false;
+        $scope.byRow = true;
+        $scope.sources = _.sortByNat($scope.sources, function(sample) { 
+            var string = sample.container.toString() + '_' + sample.position.substring(1,3) + '_' +sample.position.substring(0,1);
+            return string;
+        });
+    }
+
+    $scope.reset_sources = function reset_sources () {
+        $scope.sources = $scope.sources_init;
+    }
 
     $scope.distribute = function distribute() {
+        /** distribute source samples onto targets using various distribution options **/
+        console.log('distribute');
 
+        $scope.targets = $.extend(true, [], $scope.sources);
+
+        for (var i=0; i<$scope.targets.length; i++) {
+            $scope.targets[i]['container'] = 'new';
+        }
+         
+        console.log("Targets: " + JSON.stringify($scope.targets));
+        
+    }
+
+    $scope.testXfer = function testXfer () {
+        var Targets = [{"id":200,"position":"A1","container":1000},{"id":201,"position":"A2","container":1000},{"id":202,"position":"A3","container":1000},{"id":203,"position":"B1","container":1000},{"id":204,"position":"B2","container":1000},{"id":205,"position":"B3","container":1000},{"id":206,"position":"C1","container":1000},{"id":207,"position":"C2","container":1000},{"id":208,"position":"C3","container":1000},{"id":209,"position":"D1","container":1000},{"id":210,"position":"D2","container":1000},{"id":211,"position":"D3","container":1000},{"id":212,"position":"A1","container":1001},{"id":213,"position":"A2","container":1001},{"id":214,"position":"A3","container":1001},{"id":215,"position":"B1","container":1001},{"id":216,"position":"B2","container":1001},{"id":217,"position":"B3","container":1001},{"id":218,"position":"C1","container":1001},{"id":219,"position":"C2","container":1001},{"id":220,"position":"C3","container":1001},{"id":221,"position":"D1","container":1001},{"id":222,"position":"D2","container":1001},{"id":223,"position":"D3","container":1001},{"id":224,"position":"A1","container":1002},{"id":225,"position":"A2","container":1002},{"id":226,"position":"A3","container":1002},{"id":227,"position":"B1","container":1002},{"id":228,"position":"B2","container":1002},{"id":229,"position":"B3","container":1002},{"id":230,"position":"C1","container":1002},{"id":231,"position":"C2","container":1002},{"id":232,"position":"C3","container":1002},{"id":233,"position":"D1","container":1002},{"id":234,"position":"D2","container":1002},{"id":235,"position":"D3","container":1002},{"id":236,"position":"A1","container":1003},{"id":237,"position":"A2","container":1003},{"id":238,"position":"A3","container":1003},{"id":239,"position":"B1","container":1003},{"id":240,"position":"B2","container":1003},{"id":241,"position":"B3","container":1003},{"id":242,"position":"C1","container":1003},{"id":243,"position":"C2","container":1003},{"id":244,"position":"C3","container":1003},{"id":245,"position":"D1","container":1003},{"id":246,"position":"D2","container":1003},{"id":247,"position":"D3","container":1003},{"id":248,"position":"A1","container":1004},{"id":249,"position":"A2","container":1004},{"id":250,"position":"A3","container":1004},{"id":251,"position":"B1","container":1004},{"id":252,"position":"B2","container":1004},{"id":253,"position":"B3","container":1004},{"id":254,"position":"C1","container":1004},{"id":255,"position":"C2","container":1004},{"id":256,"position":"C3","container":1004},{"id":257,"position":"D1","container":1004},{"id":258,"position":"D2","container":1004},{"id":259,"position":"D3","container":1004},{"id":260,"position":"A1","container":1005},{"id":261,"position":"A2","container":1005},{"id":262,"position":"A3","container":1005},{"id":263,"position":"B1","container":1005},{"id":264,"position":"B2","container":1005},{"id":265,"position":"B3","container":1005},{"id":266,"position":"C1","container":1005},{"id":267,"position":"C2","container":1005},{"id":268,"position":"C3","container":1005},{"id":269,"position":"D1","container":1005},{"id":270,"position":"D2","container":1005},{"id":271,"position":"D3","container":1005},{"id":272,"position":"A1","container":1006},{"id":273,"position":"A2","container":1006},{"id":274,"position":"A3","container":1006},{"id":275,"position":"B1","container":1006},{"id":276,"position":"B2","container":1006},{"id":277,"position":"B3","container":1006},{"id":278,"position":"C1","container":1006},{"id":279,"position":"C2","container":1006},{"id":280,"position":"C3","container":1006},{"id":281,"position":"D1","container":1006},{"id":282,"position":"D2","container":1006},{"id":283,"position":"D3","container":1006},{"id":284,"position":"A1","container":1007},{"id":285,"position":"A2","container":1007},{"id":286,"position":"A3","container":1007},{"id":287,"position":"B1","container":1007},{"id":288,"position":"B2","container":1007},{"id":289,"position":"B3","container":1007},{"id":290,"position":"C1","container":1007},{"id":291,"position":"C2","container":1007},{"id":292,"position":"C3","container":1007},{"id":293,"position":"D1","container":1007},{"id":294,"position":"D2","container":1007},{"id":295,"position":"D3","container":1007}];
+
+        var data = { Sources: [{ id: 1}], Targets: [{ index: 1, position: 'A2', volume: 2, volume_units: 'ml'}], Options: { 'target_format' : 3} };
+
+        console.log("POSTING DATA: " + JSON.stringify(data));
+        $http.post("/xfer", data);
+    }
+
+    $scope.resort = function resort() {
         var sources = $scope.sources;
 
         //var target_format_id = transfer_parameters['target_format_id'];
