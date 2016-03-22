1980c9a897940c3178553c07d731824e88766e2f
Ran <ran.guin@gmail.com>
2016-03-22 14:04:10 -0700

    add growing & mini headers

--- assets/js/dependencies/custom.js
+++ assets/js/dependencies/custom.js
@@ -6,6 +6,19 @@ $(document).ready(function() {
           document.getElementsByClassName('header')[0].style.backgroundColor = '#238';
     }, 0);  
     
+    setTimeout(function sunrise () {
+	// *** final color for header animation *** //
+          document.getElementsByClassName('mini-header')[0].style.backgroundColor = '#238';
+          document.getElementsByClassName('mini-header')[0].style.padding = '5px 50px 5px 100px';
+    }, 0);  
+    
+    setTimeout(function sunrise () {
+	// *** final color for header animation *** //
+          document.getElementsByClassName('grow-header')[0].style.backgroundColor = '#238';
+          document.getElementsByClassName('grow-header')[0].style.padding = '90px';
+    }, 0);  
+    
+    
     $('#searchResults').DataTable();
 
 
