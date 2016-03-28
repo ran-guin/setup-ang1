b7185c56f4dd8757f7fd442a6fcde47809c44cce
Ran <ran.guin@gmail.com>
2016-03-27 20:50:59 -0700

    only set applicable headers

--- assets/js/dependencies/custom.js
+++ assets/js/dependencies/custom.js
@@ -3,19 +3,26 @@ $(document).ready(function() {
     
     setTimeout(function sunrise () {
 	// *** final color for header animation *** //
-          document.getElementsByClassName('header')[0].style.backgroundColor = '#238';
+          var h1 = document.getElementsByClassName('header');
+          if (h1.length > 0) { h1[0].style.backgroundColor = '#238' }
     }, 0);  
     
     setTimeout(function sunrise () {
 	// *** final color for header animation *** //
-          document.getElementsByClassName('mini-header')[0].style.backgroundColor = '#238';
-          document.getElementsByClassName('mini-header')[0].style.padding = '5px 50px 5px 100px';
+          var h2 = document.getElementsByClassName('mini-header');
+          if (h2.length > 0) { 
+            h2[0].style.backgroundColor = '#238';
+            h2[0].style.padding = '5px 50px 5px 100px';
+          }
     }, 0);  
     
     setTimeout(function sunrise () {
 	// *** final color for header animation *** //
-          document.getElementsByClassName('grow-header')[0].style.backgroundColor = '#238';
-          document.getElementsByClassName('grow-header')[0].style.padding = '90px';
+          var h3 = document.getElementsByClassName('grow-header');
+          if (h3.length > 0) {
+            h3[0].style.backgroundColor = '#238';
+            h3[0].style.padding = '90px';
+          }
     }, 0);  
     
     
