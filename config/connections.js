e7b585c39997a5a35fe7c7ba582b176fbb2571a9
Ran <ran.guin@gmail.com>
2016-04-08 10:02:55 -0700

    use env variables for DB connection

--- config/connections.js
+++ config/connections.js
@@ -40,12 +40,12 @@ module.exports.connections = {
   * Run: npm install sails-mysql                                             *
   *                                                                          *
   ***************************************************************************/
-  someMysqlServer: {
+  mysqlDB: {
     adapter: 'sails-mysql',
-    host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-    user: 'YOUR_MYSQL_USER',
-    password: 'YOUR_MYSQL_PASSWORD',
-    database: 'YOUR_MYSQL_DB'
+    host:     process.env.MYSQL_HOST,
+    user:     process.env.DEFAULT_MYSQL_USER,
+    password: process.env.DEFAULT_MYSQL_PASSWORD,
+    database: process.env.DEFAULT_MYSQL_DATABASE
   },
 
   /***************************************************************************
