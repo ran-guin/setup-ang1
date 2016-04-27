33b45de28ada85e1f660dcc5f8f4cbec93ce5497
Ran <ran.guin@gmail.com>
2016-04-27 16:22:33 -0700

    add container format api files

---
+++ api/models/Container_format.js
@@ -0,0 +1,23 @@
+/**
+* Container_format.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+	migrate: 'safe',
+	tableName: 'Plate_Format',
+	/** LEGACY **/
+
+	attributes: {
+/*
+		format : { type : 'string' },
+		wells : { type : 'integer' },
+		description : { type : 'string' }
+*/
+	}
+
+};
+
