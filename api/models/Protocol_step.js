f95569725ebd58b4bd5d0b4b36127e9b3131d210
Ran <ran.guin@gmail.com>
2016-05-02 11:06:23 -0700

    add transfer_type

--- api/models/Protocol_step.js
+++ api/models/Protocol_step.js
@@ -29,31 +29,16 @@ module.exports = {
 			defaultsTo: true,
 		},
 
+		transfer_type : { 
+			type: 'string',
+			enum: ['Transfer', 'Aliquot', 'Pre-Print'],
+		},
 		createdBy : { model : 'Employee' },
 		
 		repeatable : { 
 			type : 'boolean',
 			defaultsTo: false,
 		}
-
-		/*
-		Protocol_Step_Number : { type : 'integer' },
-		Protocol_Step_Name : { type : 'string'},
-		Protocol_Step_Instructions : { type : 'string'},
-		Protocol_Step_ID : { type : 'integer' },
-		Protocol_Step_Defaults : { type : 'string'},
-		Input : { type : 'string' },
-		Scanner : { type : 'boolean', defaultsTo: '1' },
-		Protocol_Step_Message : { type : 'string'},
-		FK_Employee__ID : { type : 'integer' },
-		Protocol_Step_Changed : { type : 'datetime' },
-		Input_Format : { type : 'string'},
-		FK_Lab_Protocol__ID : { type : 'integer' },
-		FKQC_Attribute__ID : { type : 'integer' },
-		QC_Condition : { type : 'string'},
-		Validate : { type : 'string' },
-		Repeatable : { type : 'string', enum: ['Yes','No',''], defaultsTo: ''}
-		*/
 	}  
 };
 
