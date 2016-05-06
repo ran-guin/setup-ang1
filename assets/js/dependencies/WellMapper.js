aea75f4b15ec877a1077e5792dafa6448041e294
Ran Guin <rguin@Rans-iMac.local>
2016-05-06 00:29:07 -0700

    update unit tests and tweak code

--- assets/js/dependencies/WellMapper.js
+++ assets/js/dependencies/WellMapper.js
@@ -7,7 +7,7 @@ function wellMapper() {
     // ***********
     //
     // This generates a default colour map of NxN matrices for visualization purposes
-    // (designed to illustrate mapping of samples on an 8x12 well plate using various shades of spectrum colours)
+    // (designed to illustrate mapping of sources on an 8x12 well plate using various shades of spectrum colours)
     //
     //
     //  Options: 
@@ -178,110 +178,248 @@ function wellMapper() {
 
     },
 
-    this.distribute = function ( sources, target, options ) {
+    /* Get next available position */
+    this.next_position = function (x, y, target_index) {
+        if (this.fill_by == 'Row') {
+            // console.log("fill by row to " + this.x_max + this.y_max);
+            y++;
+            if (y > this.y_max) {
+                y = 1;
+                                    
+                if (x == this.x_max) {
+                    x=this.x_min;
+                    y = this.y_min;
+                    target_index++;       // next plate ... 
+                }
+                else {
+                    x = String.fromCharCode(x.charCodeAt(0) + 1);
+                }
+            }
+        }
+        else {
+            // console.log("fill by col to " + this.x_max + this.y_max);
+            if (x == this.x_max) {
+                x=this.x_min;
+                if (y >= this.y_max) {
+                    y=this.y_min;
+                    target_index++;       // next plate ... 
+                }
+                else {
+                    y++;
+                }
+            }   
+            else {
+                x = String.fromCharCode(x.charCodeAt(0) + 1);
+            }
+
+        }
+        // console.log("next: " + x + y + ' [ ' + target_index + ' ]');
+        return { x: x, y: y, target_index: target_index};
+    }
+
+    this.initialize = function ( Target ) {
+       //var sources = [{ id: 1, type: 'blood', position: 'A1'}, { id : 2, type : 'blood', position : 'A2'}];
+        
+        var rows = Target.rows || this.source_rows;  // unnecessary..
+        var cols = Target.cols || this.source_cols;
+
+        this.fill_by = Target.fillBy || 'Row';
+        this.pack = Target.pack || false;
+        this.preserve_position = ! this.pack; // unnecessary... 
+        this.split_mode = Target.split_mode || 'parallel';
+
+        this.x_min = Target.min_row || 'A';
+        this.x_max = Target.max_row || 'A';
+        this.y_min = Target.min_col || 1;
+        this.y_max = Target.max_col || 1;
+
+        this.splitX = Target.split || Target.splitX || 1;
+
+        console.log("\nsplit = " + this.splitX + "\nfillBy = " + this.fill_by + "\npack = " + this.pack + "\nmode = " + this.split_mode + "\ntarget_size = " + this.x_max + this.y_max+ "\n");
+    }
+
+    this.distribute = function ( sources, Target ) {
 
         var targetMap = [];
-        if (! options) { options = {} };
-        if (! target || ! sources) { 
+        if (! Target) { Target = {} };
+        if (! sources) { 
             console.log("Missing Source or Target information");
             return {}
         }
 
         console.log("run distribute function... ");
-        console.log("Sources: " + JSON.stringify(sources));
-        console.log("Target: " + JSON.stringify(target));
-        console.log("Options: " + JSON.stringify(options));
+        console.log(sources.length + " Sources: " + JSON.stringify(sources));
+        console.log("Target: " + JSON.stringify(Target));
 
-        //var sources = [{ id: 1, type: 'blood', position: 'A1'}, { id : 2, type : 'blood', position : 'A2'}];
-        var rows = target.rows || this.source_rows;
-        var cols = target.cols || this.source_cols;
+        this.initialize(Target);
 
-        var fill_by = options.fillBy || 'Row';
-        var pack = options.pack || false;
-        var preserve_position = ! pack;
+        var x = this.x_min;  
+        var y = this.y_min;  
 
-        var x_min = rows[0];
-        var x_max = rows[rows.length-1];
-        var y_min = cols[0];
-        var y_max = cols[cols.length-1];
-
-        var x = x_min;  
-        var y = y_min;  
-
-/*
-        var array = [];
-        var rearray = [];
-
-        var map = {};
-        var Target = [];
-        var Colour = [];
-
-        var target_index = 0;
-        var targets = ['T0:']; // CUSTOM TEST
-*/
-        var Target = [];
+        var Transfer = [];
         var Colour = [];
         var target_index = 0;
-        var target_position = x_min + y_min.toString();
+        var target_position = this.x_min + this.y_min.toString();
 
-        Target[target_index] = {};
-        Target[target_index][target_position] = sources[0];
+        Transfer[target_index] = {};
+        Transfer[target_index][target_position] = sources[0];
 
         Colour[target_index] = {};
         Colour[target_index][target_position] = this.rgbList[0];
 
+        var repeat_set = 1;
+        var repeat_wells = 1;
 
-        for (var i=1; i<sources.length; i++) {
-            if (fill_by == 'Row') {
-                        console.log("fill by row");
-                y++;
-                if (y > y_max) {
-                    y = 1;
-                                        
-                    if (x == x_max) {
-                        x=x_min;
-                        y = y_min;
-                        target_index++;       // next plate ... 
-                    }
-                    else {
-                        x = String.fromCharCode(x.charCodeAt(0) + 1);
+        if ( ! this.pack ) {
+            // positions are static ... simply copy plates (x split if necessary)
+        }
+        else {
+
+            if (this.split_mode === 'serial') { 
+                repeat_wells = this.splitX;
+            }
+            else { 
+                repeat_set = this.splitX;
+            }
+
+            for (var h=1; h<=repeat_set; h++) {  // only repeats when split in parallel mode
+
+                for (var i=0; i<sources.length; ) {
+
+                    for (var j=1; j<=this.pack && i<sources.length; j++) {   // force use of consecutive wells if pack > 1 (even if in serial mode)
+                        for (var k=1; k<=repeat_wells && i<sources.length; k++) {
+
+                            var target_position = x + y.toString();
+                            
+                            if (! Transfer[target_index]) { Transfer[target_index] = {} }
+                            if (! Colour[target_index]) { Colour[target_index] = {} }
+
+                            console.log('box #' + target_index + '-' + target_position + ' = container #' + sources[i].id + ' from ' + sources[i].position);
+
+                            Transfer[target_index][target_position] = sources[i];
+                            Colour[target_index][target_position] = this.rgbList[i];
+                            //rearray.push([sources[i], Container.position(sources[i]), targets[target_index], target_position]);
+
+                            var next = this.next_position(x,y,target_index);
+                            x = next.x;
+                            y = next.y;
+                            target_index = next.target_index;
+                        }
+                        i = i+1;
                     }
                 }
             }
-            else {
-                        console.log("fill by col");
-                if (x == x_max) {
-                    x=x_min;
-                    if (y >= y_max) {
-                        y=y_min;
-                        target_index++;       // next plate ... 
-                    }
-                    else {
-                        y++;
-                    }
-                }   
-                else {
-                    x = String.fromCharCode(x.charCodeAt(0) + 1);
-                }
-
-            }
-
-            var target_position = x + y.toString();
-            
-            if (! Target[target_index]) { Target[target_index] = {} }
-            if (! Colour[target_index]) { Colour[target_index] = {} }
-
-            Target[target_index][target_position] = sources[i];
-            Colour[target_index][target_position] = this.rgbList[i];
-
-            //rearray.push([sources[i], Container.position(sources[i]), targets[target_index], target_position]);
         }
 
-        this.Targets = Target;
-        this.TargetMap = Colour;
+        this.Transfer = Transfer;
+        this.TransferMap = Colour;
 
-
-        return { Sources: sources, Targets : Target, TargetMap : Colour };
+        return { Transfer : Transfer, TransferMap : Colour };
     }
 
+    this.testFunction = function (number) {
+        console.log('ok');
+        return number*2;
+    }
+
+    this.testMap = function (sources, Target) {
+        // used simply to generate easily testable strings showing sample distributions
+        // this is used by unit tests below to test ordered list of ids, source positions, target_positions
+
+        var results = this.distribute(sources, Target);
+        
+        var id_list = [];
+        var position_list = [];
+        var targets = [];
+
+        for (var i=0; i<this.Transfer.length; i++) {
+            var keys = Object.keys(this.Transfer[i]);
+            for (var j=0; j<keys.length; j++) {
+                var source = this.Transfer[i][keys[j]];
+                id_list.push(source.id);
+                position_list.push(source.position);
+                var target_position = i + '-' + keys[j];
+                targets.push(target_position);
+            }
+        }
+
+        return [ id_list.join(','), position_list.join(','), targets.join(',') ];
+    }
 }
+
+/* Unit Tests */
+
+var assert = require('chai').assert;
+
+describe('wellMapper()', function() {
+ 
+    describe('* initialization', function () {
+        var map = new wellMapper();
+        map.initialize( { min_col : 3, max_row : 'D', max_col : 7 } );
+
+        it('simple initialization', function () {
+            assert.equal('A', map.x_min);
+            assert.equal('D', map.x_max);
+            assert.equal(3,   map.y_min);
+            assert.equal(7,   map.y_max);
+
+        });
+    });
+
+    // quite extensive set of tests to ensure distribution of wells follows expectations
+    describe('* distribute', function () {
+
+        var Target = { max_row : 'B', max_col : 3 };
+    
+        var ids = [1,2,3,4,5,6]
+        var positions = ['A1','A2','A3','B1','B2','B3'];
+
+        var sources = [];
+        for (var i=0; i<6; i++) {
+            sources.push( { id: ids[i], position: positions[i] });
+        }
+
+        describe('= default', function () {
+
+            var map = new wellMapper();
+            var test1 = map.testMap(sources, { pack: 1, max_row : 'B', max_col : 2 });
+
+            // target options:  transfer_qty, transfer_qty_units, format: N, sample_type: N, split: N, pack_wells: N, split_mode: serial/parallel,  
+
+            it('simple 1 to 1 default transfer', function () {
+              assert.equal('1,2,3,4,5,6', test1[0]);
+              assert.equal('A1,A2,A3,B1,B2,B3', test1[1]);
+              assert.equal('0-A1,0-A2,0-B1,0-B2,1-A1,1-A2', test1[2])
+            });
+        });
+
+       describe('= parallel split', function () {
+
+            var map = new wellMapper();
+            var test1 = map.testMap(sources, { pack: 1, split: 2, max_row : 'B', max_col : 2 });
+
+            // target options:  transfer_qty, transfer_qty_units, format: N, sample_type: N, split: N, pack_wells: N, split_mode: serial/parallel,  
+
+            it('parallel split: A,B,C.. A,B,C', function () {
+              assert.equal('1,2,3,4,5,6,1,2,3,4,5,6', test1[0]);
+              assert.equal('A1,A2,A3,B1,B2,B3,A1,A2,A3,B1,B2,B3', test1[1]);
+              assert.equal('0-A1,0-A2,0-B1,0-B2,1-A1,1-A2,1-B1,1-B2,2-A1,2-A2,2-B1,2-B2', test1[2])
+            });
+        });
+
+        describe('= serial split', function () {
+
+            var map = new wellMapper();
+            var test1 = map.testMap(sources, { pack: 1, split: 2, split_mode: 'serial', max_row : 'B', max_col : 2 });
+
+            // target options:  transfer_qty, transfer_qty_units, format: N, sample_type: N, split: N, pack_wells: N, split_mode: serial/parallel,  
+
+            it('serial split: A,A,B,B,C,C...', function () {
+              assert.equal('1,1,2,2,3,3,4,4,5,5,6,6', test1[0]);
+              assert.equal('A1,A1,A2,A2,A3,A3,B1,B1,B2,B2,B3,B3', test1[1]);
+              assert.equal('0-A1,0-A2,0-B1,0-B2,1-A1,1-A2,1-B1,1-B2,2-A1,2-A2,2-B1,2-B2', test1[2])
+            });
+        });
+
+    });
+});
