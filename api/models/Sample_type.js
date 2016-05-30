03f42c5671436d096336d9e2c3239847e806a801
Ran <ran.guin@gmail.com>
2016-05-30 01:25:31 -0700

    add baseline model

---
+++ api/models/Sample_type.js
@@ -0,0 +1,23 @@
+/**
+* Sample_type.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+	tableName: 'Sample_Type',
+	migrate: 'safe',
+	/* LEGACY */
+
+	attributes: {
+		//
+	},
+
+	alias: {
+		'id' : 'Sample_Type_ID',
+		'name' : 'Sample_Type'
+	},	  
+};
+
