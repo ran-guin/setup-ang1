2b99d8d618d09d9ec17ea891d2f79c10b4d7d3c3
Ran <ran.guin@gmail.com>
2016-06-01 16:21:01 -0700

    add loadSteps & parse_steps

--- api/models/Protocol_step.js
+++ api/models/Protocol_step.js
@@ -5,6 +5,8 @@
 * @docs        :: http://sailsjs.org/#!documentation/models
 */
 
+var q = require('q');
+
 module.exports = {
 	
 	attributes: {
@@ -47,6 +49,131 @@ module.exports = {
 		pack_wells 	:  0,
 		fill_by 	: 'row',
 		split_mode	: 'parallel'
+	},
+
+
+	'loadSteps' : function (protocol_id) {
+		
+		var deferred = q.defer();
+
+		if (protocol_id) {
+
+			var query = "SELECT * FROM protocol_step"
+				+ " LEFT JOIN Plate_Format ON Plate_Format_ID = Target_format"
+				+ " WHERE Lab_protocol = " + protocol_id
+				+ " GROUP BY protocol_step.id"
+				+ " ORDER BY protocol_step.step_number";
+
+
+			console.log("\n** Protocol Step : " + query);
+			Record.query_promise(query)
+			.then ( function (stepData) {
+				Protocol_step.parse_steps( stepData )
+				.then ( function ( data ) {
+
+					var send = { 
+			    		Steps : stepData, 
+			    		attributes: data.attributes,
+			    	};
+
+					deferred.resolve(send);
+				})
+				.catch ( function (err) {
+					deferred.reject(err);
+				});
+
+			})
+			.catch ( function (err) {
+				deferred.reject(err);
+			});		
+		}
+		else {
+			deferred.reject("no protocol defined");
+		}
+
+		return deferred.promise;
+	},
+
+	// simply parses query results from 'loadSteps function '
+	'parse_steps' : function (query_result) {
+		// returns { input: [array of input strings], attributes: [array of attributes] }
+		var deferred = q.defer();
+
+		var Input = [];
+		var Attributes = [];
+
+		var inputArray = [];
+		var Plate_attributes = [];
+		var Prep_attributes = [];
+
+		var deferred = q.defer();
+
+		var staticInput = ['Split', 'Prep_Comments'];
+
+		// console.log("QR: " + JSON.stringify(query_result));
+
+		var list = [];
+ 		for (i=0; i<query_result.length; i++) {
+			var input = query_result[i]['input_options'];
+			if (input) { 				var stepInput = input.split(':');
+				list = _.union(list, stepInput);
+			}
+		}
+
+		console.log("List: " + list.join(','));
+		var ordered = ['transfer_qty','Split','solution','solution_qty','equipment'];
+		var last    = ['location','comments'];
+		var orderedList = [];
+		var attributeList = [];
+		// reorder list in standard order for normal input //
+
+		var Included = {};
+		for (var i=0; i<ordered.length; i++) {
+			if ( list.indexOf(ordered[i]) > 0 ) {
+				orderedList.push(ordered[i])
+				Included[ordered[i]] = 1;
+			}
+		}
+		for (var i=0; i<list.length; i++) {
+			if ( Included[list[i]]) {
+				console.log('already included ' + list[i]);
+			}
+			else if ( last.indexOf(list[i]) > 0 ) { 
+				console.log('moving to back: ' + list[i]);
+			}
+			else {
+				attributeList.push(list[i]);
+				orderedList.push(list[i]);
+			}
+		}
+
+		for (var i=0; i<last.length; i++) {
+			if ( list.indexOf(last[i]) ) {
+				orderedList.push(last[i])
+				Included[last[i]] = 1;
+			}
+		}
+
+		console.log("Reordered List: " + orderedList.join(', '));
+		// Legacy fields specified //
+		var fields = "Attribute_ID as id, Attribute_Class as model, Attribute_Name as name, Attribute_Type as type, Attribute_Format as format"; // legacy 
+		var query = 'SELECT ' + fields + " FROM Attribute WHERE Attribute_Class IN ('Plate','Prep') AND Attribute_Name IN ('" 
+			+ attributeList.join("','")
+			+ "')";
+
+		console.log("\n** SQL: " + query);
+
+		Record.query_promise(query)
+		.then (function (attributeData) {
+			console.log("Attributes: " + JSON.stringify(attributeData))
+			deferred.resolve({ 'input' : orderedList, 'attributes' : attributeData});
+		})
+		.catch (function (err) {
+			deferred.reject("error looking for Attributes"); 
+		})
+
+		console.log("Union: " + JSON.stringify(list));
+		return deferred.promise;
 	},
 };
 
