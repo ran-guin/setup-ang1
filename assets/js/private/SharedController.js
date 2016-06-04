a5f91775c853b7ad284261ee728b16ed0092e68b
Ran <ran.guin@gmail.com>
2016-06-03 19:58:33 -0700

    change old CommonController to SharedController

---
+++ assets/js/private/SharedController.js
@@ -0,0 +1,109 @@
+var app = angular.module('myApp');
+
+app.controller('SharedController', 
+    ['$scope', '$q', '$rootScope', '$http', '$location', 
+    function ($scope, $q, $rootScope, $http, $location) {
+
+        console.log('loaded Common Controller');
+
+        $scope.messages = [];
+        $scope.warnings = [];
+        $scope.errors   = [];
+
+        // Automatically generate timestamp attribute along with standard attributes for lastMonth & nextMonth //
+        var start = new Date();
+        $scope.timestamp = start.toISOString().slice(0, 19).replace('T', ' '); 
+            
+        $scope.lastMonth = new Date(start.getTime() - 30 * 24 * 60 * 60 * 1000 ).toISOString();
+        $scope.nextMonth = new Date(start.getTime() + 30 * 24 * 60 * 60 * 1000 ).toISOString();
+
+        /** timer with date + hours + minutes - automatically updates  **/
+        var update_seconds = 1;
+        setInterval (function() {
+            var now = new Date();
+            $scope.now = now;
+
+            $scope.timestamp = now.toISOString().slice(0, 19).replace('T', ' ');
+            $scope.created = now.toISOString().slice(0, 19).replace('T', ' ');
+
+            $scope.$apply();
+        }, update_seconds*1000);
+   
+
+        $scope.parse_messages = function ( result ) {
+
+            if (result && result.data) {
+
+                // below clears current messages if no new messages generated
+                if (result.messages) { $scope.messages = result.messages }
+                if (result.warnings) { $scope.warnings = result.warnings }
+                if (result.errors)     { $scope.errors = result.errors }
+
+                console.log("Actual result" + JSON.stringify(result));
+                console.log("Parsed messages: " + $scope.messages.join(',') );
+                
+                return result.data;
+            }
+            else {
+                console.log("Not is formatted form");
+                return result;
+            }
+        }
+
+        $scope.padded = function (view) {
+            return view;
+            //return "\n<div class='container' style='padding:20px'>\n" + view + "</div>\n";
+        }
+        
+        $scope.injectData = function (url, element, ids ) {
+            console.log("INJECT HTML");
+            if (! element) { element = 'injectedData' }
+
+            url = url + '?element=' + element;
+            if (ids) { url = url + '&ids=' + ids }
+
+
+            var el = document.getElementById(element);
+            if (el) {
+                console.log("Calling: " + url);
+                $http.get(url)
+                .then ( function (result) {
+                    console.log("Got API Data...");
+
+                    console.log(JSON.stringify(result));
+                    console.log(JSON.stringify(result.data));
+                    el.innerHTML = $scope.padded( result.data );
+                    //el.html($scope.padded( result.data));
+                    $scope[element]  = true;
+                })
+                .catch ( function (err) {
+                    console.log("Error getting injection data: " + JSON.stringify(err));
+                });
+            }
+            else {
+                console.log("element: " + element + ' NOT FOUND !');
+            }
+        }
+
+        $scope.injectedAlready = function (element) {
+            if (! element) { element = 'injectedData' }
+            var el = document.getElementById(element);
+
+            if (el && el.innerHTML.length ) { returnVal = true }
+            else { returnVal = false }
+
+            // console.log("check " + element + ' = ' + returnVal + ':' + el.innerHTML.length)
+            
+            return returnVal;
+        }
+
+        $scope.uninjectData = function (element) {
+            if (! element) { element = 'injectedData' }
+            var el = document.getElementById(element);
+            if (el) { el.innerHTML = '' }
+            else { console.log("could not close " + element) }
+            $scope[element] = false;
+        }
+
+
+}]);
