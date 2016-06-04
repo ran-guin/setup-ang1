c2d49cf40b73e9c6097e1f8de4ebc977dd10640a
Ran <ran.guin@gmail.com>
2016-06-03 18:17:42 -0700

    include last_step data when generating Container main page

--- api/controllers/BarcodeController.js
+++ api/controllers/BarcodeController.js
@@ -6,6 +6,7 @@
  */
 
 var bodyParser = require('body-parser');
+var _ = require('underscore-node');
 
 module.exports = {
 	
@@ -23,6 +24,8 @@ module.exports = {
 		.then ( function (Scanned) {
 			if (Scanned['Plate'].length) {
 				var ids = Scanned['Plate'];
+
+				console.log("Load: " + ids.join(','));
 				Container.loadData(ids)
 				.then (function (data) {
 
@@ -32,27 +35,33 @@ module.exports = {
 					var sampleList = [];
 					if (data.length == 0) {
 						errorMsg = "expecting ids: " + ids.join(', ') + "<P>... but No Containers Found (?)";
+						return res.render('customize/private_home');
 					}	
 					else {
 						for (var i=0; i<data.length; i++) {
 							sampleList.push(data[i].id);
 						}
-					}	
 
-					if (sampleList.length < ids.length) { 
-						warningMsg = "Scanned " + ids.length + " records but only found " + sampleList.length;
+						if (sampleList.length < ids.length) { 
+							warningMsg = "Scanned " + ids.length + " records but only found " + sampleList.length;
+						}
+
+						var get_last_step = Protocol_Step.parse_last_step(data);
+						var last_step = get_last_step.last_step;
+						if (get_last_step.warning) { warningMsg = get_last_step.warning }
+
+						return res.render('lims/Container', { 
+							plate_ids: ids.join(','), 
+							last_step : last_step, 
+							Samples: data , 
+							//sampleList : sampleList,
+							message : 'loaded Matrix Tube(s)',
+							warningMsg: warningMsg,
+							errorMsg : errorMsg,
+							//target_formats : target_formats 
+						});
 					}
 
-					return res.render('lims/Container', { 
-						plate_ids: ids.join(','), 
-						//protocols : Protocols, 
-						Samples: data , 
-						//sampleList : sampleList,
-						message : 'loaded Matrix Tube(s)',
-						warningMsg: warningMsg,
-						errorMsg : errorMsg,
-						//target_formats : target_formats 
-					});
 				})
 				.catch (function (err) {
 					return res.render('customize/private_home', {errorMsg: JSON.stringify(err) });
