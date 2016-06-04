f2a3ca8fcaafb2c2f2060ad2d7192faeb27227fa
Ran <ran.guin@gmail.com>
2016-06-03 23:10:34 -0700

    pass options to Attribute clone

--- api/models/Record.js
+++ api/models/Record.js
@@ -337,7 +337,7 @@ module.exports = {
 							var nextId = target_id + i;
 							target_ids.push(nextId);
 						}
-						Attribute.clone(table, ids, target_ids)
+						Attribute.clone(table, ids, target_ids, resetData, options)
 						.then (function (data1) {
 							console.log("attribute update: " + JSON.stringify(data1));
 							deferred.resolve({ data: data, insertIds: target_ids, created: newResponse, attributes: data1});
