4bcb43d10cb233b184058ef60ec41061d4c52a7b
Ran <ran.guin@gmail.com>
2016-06-03 18:01:28 -0700

    separate FancyForm angular functionality

---
+++ assets/js/private/FancyFormController.js
@@ -0,0 +1,141 @@
+var app = angular.module('myApp',['ngFileUpload']);
+
+app.controller('FancyFormController', 
+    ['$scope', '$q', '$rootScope', '$http', '$location', 'FancyFormFactory', 'Upload', 
+    function ($scope, $q, $rootScope, $http, $location, FancyFormFactory, Upload) {
+        console.log('loaded Fancy Form Controller');
+        
+        // Support Basic Password Validation and Confirmation
+        // usage: ng-model='repeat' ng-key-up="compare(repeat)"
+
+	$scope.custom_disable = false;
+
+        $scope.testUnique = function (element, model, field) {
+            console.log("CHECK UNIQUENESS");
+            var url = "/remote_login/validate?model=" + model + '&value=' + $scope[element];
+            console.log("URL: " + url); 
+	    $http.get(url)
+            .then ( function (result) {
+                if (result[0] && result[0].count) {
+			$scope.warnings.push("not unique");
+			$scope.custom_disable = true; 
+		}
+		else { $scope.custom_disable = false }
+	    })
+            .catch ( function (err) {
+                $scope.warnings.push('could not confirm uniqueness');
+            })
+        }
+
+        $scope.passwordValidation = /^[a-zA-Z]\w{3,14}$/;
+        $scope.confirmedPassword = false;
+        $scope.compare = function (repeatEntry) {
+            console.log("Compared " + repeatEntry + " with " + $scope.password);
+            $scope.confirmedPassword = $scope.password == repeatEntry ? true : false;
+            if ($scope.confirmedPassword) { document.getElementById('confirm_password').style="color:green" }
+        }
+
+    	$scope.padded = function (view) {
+    		return view;
+    		//return "\n<div class='container' style='padding:20px'>\n" + view + "</div>\n";
+    	}
+
+        // Automatically Load Lookup Files //
+        $scope.loadLookup = function loadLookup(model, labels, prompt, condition) {
+
+            var table = model;
+         
+        	var url = "/lookup/" + table + '/';
+
+            var options = {};
+
+            if ( labels ) { 
+        		url = url + labels;   // defaults to "id:name:label"
+        		options.label = labels;
+        	}
+        	if (prompt) {
+        		options.prompt = prompt;
+        	}
+        	if (condition) {
+        		options.condition = condition;
+        	}
+            
+        	url = url + '?';
+            console.log("call factory lookup with url: " + url + ':' + JSON.stringify(options));
+       		var got = FancyFormFactory.loadLookup(url, table, options);
+
+       		console.log("Loaded " + table + " Lookup Table");
+    	}
+
+        $scope.loadAttributePrompt = function loadAttributePrompt(model, attribute, label, defaultTo) {
+        
+        	var url = "/attribute/" + model + '/' + attribute + '/';
+
+       		var got = FancyFormFactory.loadAttributePrompt(url, model, attribute, label, defaultTo);
+
+       		console.log("Loaded " + model + ': ' + attribute + " attribute prompt");
+    	}
+
+    	// Automatically generate timestamp attribute along with standard attributes for lastMonth & nextMonth //
+	    var start = new Date();
+	    $scope.timestamp = start.toISOString().slice(0, 19).replace('T', ' '); 
+	        
+	    $scope.lastMonth = new Date(start.getTime() - 30 * 24 * 60 * 60 * 1000 ).toISOString();
+	    $scope.nextMonth = new Date(start.getTime() + 30 * 24 * 60 * 60 * 1000 ).toISOString();
+
+	    /** timer with date + hours + minutes - automatically updates  **/
+	    var update_seconds = 1;
+	    setInterval (function() {
+	        var now = new Date();
+	        $scope.now = now;
+
+	        $scope.timestamp = now.toISOString().slice(0, 19).replace('T', ' ');
+	        $scope.created = now.toISOString().slice(0, 19).replace('T', ' ');
+
+	        $scope.$apply();
+	    }, update_seconds*1000);
+   
+        $scope.setup = function( config ) {
+
+	    }
+
+	    $scope.setField = function (field, value) {
+	    	console.log("SET " + field + ' to ' + value);
+	    	$scope[field] = value;
+	    }
+
+        $scope.updateLookup = function ( lookup ) {
+            var model = lookup + '-id';
+
+            var el1 = document.getElementById(model);
+
+            if (el1) {
+                $scope[lookup] = el1.value;
+                $scope[lookup + '_id'] = el1.value;
+                console.log('sync ' + lookup + ' to lookup value: ' + el1.value);
+            }
+            else {
+                console.log("Warning: Could not find " + model + " element to synchronize");
+            }
+             
+            var el2 = document.getElementById(lookup + '-label');
+
+            if (el2) {
+                $scope[lookup + '_label'] = el2.value;
+                console.log('sync label to  ' + el2.value);
+            }
+            else {
+                console.log("Warning: Could not find " + model + " element to synchronize");
+            }
+        }
+
+        $scope.updateLookups = function () {
+            var lookups = document.getElementsByClassName('lookupMenu');
+            for (var i=0; i<lookups.length; i++) {
+                console.log("update " + JSON.stringify(lookups[i]));
+                var identifier = lookups[i].id;
+                $scope.updateLookup(identifier); 
+            }
+        }
+
+}]);
