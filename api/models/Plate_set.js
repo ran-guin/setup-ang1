3077a8abe1dbb3dbb3d9d077c37ddb4a089893d0
Ran <ran.guin@gmail.com>
2016-06-21 11:14:58 -0700

    add plate_set model

---
+++ api/models/Plate_set.js
@@ -0,0 +1,16 @@
+/**
+ * Plate_set.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  tableName : 'Plate_Set',
+  migrate: 'safe',
+  attributes: {
+
+  }
+};
+
