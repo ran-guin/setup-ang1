fe07dd7ba9b80085045084b90977126f96741d07
Ran <ran.guin@gmail.com>
2016-07-08 17:59:19 -0700

    fully qualify query retrieving FK_Plate_Set__Number in case of conflict

--- api/controllers/ContainerController.js
+++ api/controllers/ContainerController.js
@@ -22,7 +22,7 @@ module.exports = {
 		var element = req.param('element') || 'injectedHistory';  // match default in CommonController
 		var include_parents = req.param('include_parents');
 
-		var fields = ['Count(DISTINCT Plate.Plate_ID) as Samples', 'Group_Concat(distinct FK_Plate_Set__Number) as Sets', 'Prep_Name as Step', 'lab_protocol.name as Protocol', 'Prep_DateTime as Completed', 'Group_Concat(DISTINCT Employee_Name) as Completed_By'];
+		var fields = ['Count(DISTINCT Plate.Plate_ID) as Samples', 'Group_Concat(distinct Plate_Prep.FK_Plate_Set__Number) as Sets', 'Prep_Name as Step', 'lab_protocol.name as Protocol', 'Prep_DateTime as Completed', 'Group_Concat(DISTINCT Employee_Name) as Completed_By'];
 		fields.push("CASE WHEN Max(Attribute_ID) IS NULL THEN '' ELSE GROUP_CONCAT( DISTINCT CONCAT(Attribute_Name,'=',Attribute_Value) SEPARATOR ';<BR>') END as attributes");
 
 		fields.push('Prep_Comments as Comments');
