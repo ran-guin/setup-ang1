ffe2a796e1578d48fb1bb5d824e5514fc67271ae
Ran Guin <rguin@Rans-iMac.local>
2016-07-31 14:29:38 -0700

    move get_plate_set to LIMS module

--- assets/js/private/lims/LIMSController.js
+++ assets/js/private/lims/LIMSController.js
@@ -8,7 +8,9 @@ function limsController ($scope, $rootScope, $http, $q) {
 
     $scope.active = {};
     $scope.active.Samples = [];
-
+ 
+    $scope.active.valid_plate_sets = [];
+    
     // Custom methods ... 
     $scope.Prefix = function Prefix (model) {
         var P = {
@@ -51,6 +53,33 @@ function limsController ($scope, $rootScope, $http, $q) {
         console.log("ACTIVE Loaded " + $scope.active.Samples.length + ' active Samples');
     }
 
+    $scope.get_plate_sets = function () {
+        var count = $scope.active.plate_ids.length;
+        console.log("using " + count + ' ids');
+
+        var condition = " FK_Plate__ID IN (" + $scope.active.plate_ids.join(',') + ") GROUP BY Plate_Set_Number HAVING COUNT(*) = " + count;
+        
+        var searchData = {
+            scope: { 'Plate_Set' : [ 'DISTINCT Plate_Set_Number as PS'] },
+            condition : condition,
+        };
+
+        $http.post('/Record/search', searchData)
+        .then ( function (result) {
+            console.log("RESULT: " + JSON.stringify(result));
+            $scope.active.valid_plate_sets = [];
+            if (result.data && result.data[0] && result.data[0][0]) {
+                for (var i=0; i<result.data[0].length; i++) {
+                    $scope.active.valid_plate_sets.push(result.data[0][i].PS);
+                }
+            }
+            console.log("Retrieved SET: " + JSON.stringify($scope.active.valid_plate_sets));
+        })
+        .catch ( function (err) { 
+            console.log("Error getting sets: " + err);
+        }); 
+    }
+
     $scope.load_plate_set = function ( options ) {
     	// use set = 'new' to create new set 
 
@@ -66,9 +95,9 @@ function limsController ($scope, $rootScope, $http, $q) {
     	}
     	else {
 	        $http.post('/Record/search', { scope : { 'Plate_Set' : [ 'Max(Plate_Set_Number) as MaxPS'] }})
-	        .then ( function (result) {
-	           if (result.data && result.data[0] && result.data[0][0]) {
-	                var maxPS = result.data[0][0].MaxPS || 1
+	        .then ( function ( result ) {
+	            if (result.data && result.data.results && result.data.results[0]) {
+	                var maxPS = result.data.results[0].MaxPS || 1
 	                console.log("SAVE SET " + JSON.stringify(maxPS));
 
 	                var data = [];
@@ -76,19 +105,23 @@ function limsController ($scope, $rootScope, $http, $q) {
 	                    var record = { FK_Plate__ID : Samples[i].id, Plate_Set_Number: maxPS+1 , FKParent_Plate_Set__Number: parent }    
 	                    data.push(record);
 	                }
-	                
-	                $http.post("Record/save", { model: 'plate_set', data: data} )
-	                .then (function (result) {
+	           
+                    console.log("Save Set: " + JSON.stringify(data));
+	                $http.post("record/save", { model: 'plate_set', data: data} )
+	                .then (function ( response ) {
 	                    $scope.active.plate_set = maxPS + 1;
 	                    console.log("SAVED Plate Set: " + $scope.active.plate_set);
-	                    $scope.valid_plate_sets.push($scope.active.plate_set);
+	                    $scope.active.valid_plate_sets.push($scope.active.plate_set);
 	                })
 	                .catch ( function (err) {
 	                    $scope.errors.push("Error saving plate set");
 	                    console.log("Error saving plate set");
 	                })
 	            }
-	            else { console.log('max ps result in incorrect format') }
+	            else { 
+                    console.log('max ps result in incorrect format');
+                    console.log(JSON.stringify(result.data));
+                }
 	        })
 	        .catch ( function (err) {
 	            console.log("Error retrieving max plate set");
