6bb2deca1e56056263ea954f3cac9e7afb25ffba
Ran <ran.guin@gmail.com>
2016-08-22 00:41:11 -0700

    new sample tracking model

---
+++ api/models/Sample_tracking.js
@@ -0,0 +1,18 @@
+/**
+* Sample_tracking.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+  	Container : { model : 'container'},
+  	Moved_from  : { model : 'rack' },
+  	Moved_to  : { model : 'rack' },
+  	moved : { type : 'datetime' },
+  	Moved_by : { model : 'user' }
+  }
+};
+
