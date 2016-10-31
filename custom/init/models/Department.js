751ec3fba17146aa0c1c84138150a8985015bde6
Ran <ran.guin@gmail.com>
2016-10-31 17:16:40 +0700

    initial state

---
+++ custom/init/models/Department.js
@@ -0,0 +1,19 @@
+/**
+* Department.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+  	name : { type : 'string' } 
+  },
+
+  initData: [
+  	{ name : 'LIMS' },
+  	{ name : 'Laboratory' }
+  ],
+};
+
