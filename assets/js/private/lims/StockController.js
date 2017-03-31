6bc3b28289a8e32a6a7e8400b15bbec33b304adc
Ran <ran.guin@gmail.com>
2017-03-30 17:15:00 -0700

    refactor stock controller to autoset Equipment Names and retrieve prefix from database

--- assets/js/private/lims/StockController.js
+++ assets/js/private/lims/StockController.js
@@ -65,40 +65,66 @@ function stockController ($scope, $rootScope, $http, $q) {
 
         $scope.reset_messages();
 
-        var EI = "Mid(Equipment.Equipment_Name, 5, 5)";
-        var q = "SELECT " + EI + " AS name from Equipment ORDER BY " + EI;
-
-        if ($scope.catalog && $scope.catalog.name) {
+        if ($scope.catalog && $scope.catalog.name && $scope.catalog.id) {
             
-            console.log("check catalog");
-            $scope.validated.catalog = true;
+           $scope.validated.catalog = true;
+            
+            if ($scope.type == 'Equipment') {
+                var url = "/remoteQuery";
+                var query = "SELECT Prefix from Stock_Catalog, Equipment_Category WHERE FK_Equipment_Category__ID=Equipment_Category_ID AND Stock_Catalog_ID = " + $scope.catalog.id;
 
-            $scope.next_in_line({ index: "Mid(Equipment.Equipment_Name, 5, 5)", table: 'Equipment', fill: false, repeat: $scope.number_in_batch, prefix: 'F80-'} )
-            .then ( function (result) {
-                $scope.name = result;
-                
-                if ($scope.number_in_batch > 1) {
-                    $scope.names = result;
-                }
-                else {
-                    delete $scope.names;
-                }
+                $http.post(url, { query : query })
+                    .then ( function (result) {   
+                    console.log(JSON.stringify(result));
 
-                console.log('Yes' + result + ': ' + $scope.name);
-                $scope.validate_stock_form();
-            })  
-            .catch ( function (err) {
-                console.log('uh oh: ' + err);
-                $scope.validate_stock_form();
-            });
+                    if (result.data.length) {         
+                        var prefix = result.data[0].Prefix + '-';
+                        var offset = prefix.length + 1;
+
+                        var condition = "Equipment_Name like '" + prefix + "%'";
+                        $scope.next_in_line(
+                            { 
+                                index: "Mid(Equipment.Equipment_Name," + offset + ',' + '6)', 
+                                table: 'Equipment', 
+                                fill: false, 
+                                condition: "Equipment_Name LIKE '" + prefix + "%'",
+                                repeat: $scope.number_in_batch, 
+                                prefix: prefix
+                            }
+                        )
+                        .then ( function (result) {
+                            $scope.name = result;
+                            
+                            if ($scope.number_in_batch > 1) {
+                                $scope.names = result;
+                            }
+                            else {
+                                delete $scope.names;
+                            }
+                        })
+                        .catch ( function (err) {
+                            console.log("Could not find next in line");
+                            $scope.validated.catalog = false;                    
+                        });
+                    }
+                    else {
+                        $scope.error("Error finding Equipment prefix");
+                    }
+                })  
+                .catch ( function (err) {
+                    console.log('Error getting Equipment prefix');
+                    $scope.validated.catalog = false;
+                });
+            }
         }
         else {
             console.log("no catalog " + $scope.catalog + ' : ' + JSON.stringify($scope.catalog));
             $scope.validated.catalog = false;
-                    
-            $scope.validate_stock_form();
 
         }
+
+        $scope.validate_stock_form();
+
     }
 
     $scope.split_serial = function () {
@@ -124,6 +150,9 @@ function stockController ($scope, $rootScope, $http, $q) {
     }
 
     $scope.save = function () {
+
+        $scope.reset_messages();
+        
         console.log($scope.received);
    
         if ($scope.expiry && $scope.expiry.constructor === Date ) { $scope.expiry = $scope.mysql_date( $scope.expiry ) }
@@ -140,9 +169,8 @@ function stockController ($scope, $rootScope, $http, $q) {
 
     	var data = { Stock : StockData, type: $scope.type };
 
-
     	if ($scope.type === 'Reagent') {
-    		var subData = {
+            var subData = {
     			'expiry'            : $scope.expiry,
     			// 'number_in_batch'   : $scope.number_in_batch,
                 'qty'               : $scope.qty,
@@ -153,8 +181,6 @@ function stockController ($scope, $rootScope, $http, $q) {
     		data['Reagent'] = subData;
     	}
         else if ($scope.type === 'Equipment') {
-
-            var name = $scope.name || 'TBD';
 
             var subData = {
                 // 'number_in_batch'   : $scope.number_in_batch,
@@ -168,65 +194,52 @@ function stockController ($scope, $rootScope, $http, $q) {
             data['Equipment'] = subData;
         }
 
-        console.log("Post Stock data: " + JSON.stringify(data));
-        
-        if ($scope.catalog && $scope.catalog.name) {
-            $scope.next_in_line({ index: "Mid(Equipment.Equipment_Name, 5, 5)", table: 'Equipment', fill: false, prefix: 'F80-'} )
-            .then ( function (result) {
-                console.log(result);
+        console.log("Post Stock data: " + JSON.stringify(data));          
 
-            	$http.post("Stock/receive", data)
-            	.then (function (result) {
-            		var response = result.data;
-                    console.log("GOT: " + JSON.stringify(result.data));
-                    
-                    if (response.error  && response.error.length ) {
-                        for (var i=0; i<response.error.length; i++) {
-                            $scope.error(response.error[i]);
-                        }
-                    }
-                    else {
-                        $scope.message("Added Stock Record(s)");
-                        $scope.injectData('/Stock/received?render=1&limit=1','rcvdStock');
-                    }
+    	$http.post("Stock/receive", data)
+    	.then (function (result) {
+    		var response = result.data;
+            console.log("GOT: " + JSON.stringify(result.data));
+            
+            if (response.error  && response.error.length ) {
+                for (var i=0; i<response.error.length; i++) {
+                    $scope.error(response.error[i]);
+                }
+            }
+            else {
+                $scope.message("Added Stock Record(s)");
+                $scope.injectData('/Stock/received?render=1&limit=1','rcvdStock');
+            }
 
-                    // add barcodes in createNew automatically ... 
+            // add barcodes in createNew automatically ... 
 
-                    var ids   = result.data.ids;
-                    var model = result.data.model;
+            var ids   = result.data.ids;
+            var model = result.data.model;
 
-                    console.log(model + ": " + ids);
+            console.log(model + ": " + ids);
 
-                    if (model && ids.length) {
-                        $scope.print_Labels(model, ids)
-                        .then ( function (response) {
-                            // $scope.message('attempted to print label(s)..');
-                            console.log("printed : " + JSON.stringify(response));
-                        })
-                        .catch ( function (err) {
-                            $scope.error('error printing barcodes');
-                            console.log("Error printing barcodes");
-                        });
-                    }
-                    else {
-                        $scope.error("Error retrieving new model or id(s)");
-                    }
-                    
-                    $scope.clear_form();
-            	})    	
-            	.catch (function (err){
-                    $scope.error("Error adding Stock Record(s)");
-            		console.log("Error receiving stock: ");
-                    console.log( err );
-            	});
-            })
-            .catch (function (err) {
-                console.log('error getting next element in list')
-            });
-        }
-        else {
-            $scope.warning("Category not yet chosen...");
-        }
+            if (model && ids.length) {
+                $scope.print_Labels(model, ids)
+                .then ( function (response) {
+                    // $scope.message('attempted to print label(s)..');
+                    console.log("printed : " + JSON.stringify(response));
+                })
+                .catch ( function (err) {
+                    $scope.error('error printing barcodes');
+                    console.log("Error printing barcodes");
+                });
+            }
+            else {
+                $scope.error("Error retrieving new model or id(s)");
+            }
+            
+            $scope.clear_form();
+    	})    	
+    	.catch (function (err){
+            $scope.error("Error adding Stock Record(s)");
+    		console.log("Error receiving stock: ");
+            console.log( err );
+    	});
     }
 
 }]);
