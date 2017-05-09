41419a0fc3186327fb2c1058236013fdd66cdf6c
Ran <ran.guin@gmail.com>
2017-05-09 15:13:42 -0700

    use production db for production mode

--- config/env/production.js
+++ config/env/production.js
@@ -18,7 +18,7 @@ module.exports = {
    ***************************************************************************/
 
   models: {
-    connection: 'testDB',
+    connection: 'productionDB',
     migrate: 'safe',
   },
 
