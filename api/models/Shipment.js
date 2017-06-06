bcbc24815d74ceb951d12a50e254d7a099e7e375
Ran Guin <ran.guin@gmail.com>
2017-06-05 17:46:49 -0700

    inherit standard Shipment object

--- api/models/Shipment.js
+++ api/models/Shipment.js
@@ -5,25 +5,31 @@
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
 
-module.exports = {
+var BaseModel = require("./../../custom/core/models/Shipment.js");
+// var BaseModel = require("./Xuser.js");
 
-	migrate: 'safe',
-	tableName: 'Shipment',
+module.exports = _.merge({}, BaseModel, {
+
+  migrate: 'safe',
+  tableName: 'Shipment',
 
   attributes: {
-  	Shipment_ID : { type: 'number' },
-  	Shipment_Received : { type: 'time' },
-  	Waybill_Number : { type: 'string' },
-  	Shipment_Comments : { type: 'text' },
-  	FKOriginal_Shipment__ID : { type: 'number' },
-  	Sample_Condition : { 
-  		type : 'string',
-  		enum: ['frozen','partially frozen','cold','room temp','warm','multiple conditions']
-  	}
+    Shipment_ID : { type: 'number' },
+    Shipment_Received : { type: 'time' },
+    Waybill_Number : { type: 'string' },
+    Shipment_Comments : { type: 'text' },
   },
 
   alias : {
-  	'id' : 'Shipment_ID',
+    'id' : 'Shipment_ID',
+    'received' : 'Shipment_Received',
+    'waybill_number' : 'Waybill_Number',
+    'comments' : 'Shipment_Comments',
+    'parent_Shipment' : 'FKOriginal_Shipment__ID',
+    'sample_condition' : 'Sample_Condition',
+    'Received_by' : 'FKRecipient_Employee__ID',
+    'Sent_by' : 'FKSender_Employee__ID'
   }
-};
+ 
+});
 
