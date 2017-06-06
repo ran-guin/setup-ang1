152bcd9ada80abeffe08c1077d2d399e08455414
Ran Guin <ran.guin@gmail.com>
2017-06-05 17:48:12 -0700

    use prototype

--- custom/lims/models/Shipment.js
+++ custom/lims/models/Shipment.js
@@ -5,35 +5,23 @@
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
 
-module.exports = {
+var BaseModel = require("./../../custom/core/models/Shipment.js");
+// var BaseModel = require("./Xuser.js");
 
-	migrate: 'safe',
-	tableName: 'Shipment',
+module.exports = _.merge({}, BaseModel, {
+
+  migrate: 'safe',
+  tableName: 'Shipment',
 
   attributes: {
-    // received : { type: 'time' },
-    // received_by : { model : 'user' },
-    // waybill_number : { type: 'string' },
-    // comments : { type: 'text' },
-    // parent_Shipment : { type: 'int' }, // or { model: 'shipment'}
-    // sample_condition : { 
-    //  type : 'string',
-    //  enum: ['frozen','partially frozen','cold','room temp','warm','multiple conditions']
-    // },
-
-  	Shipment_ID : { type: 'number' },
-  	Shipment_Received : { type: 'time' },
-  	Waybill_Number : { type: 'string' },
-  	Shipment_Comments : { type: 'text' },
-  	// FKOriginal_Shipment__ID : { type: 'number' },
-  	// Package_Conditions : { 
-  	// 	type : 'string',
-  	// 	enum: ['frozen','partially frozen','cold','room temp','warm','multiple conditions']
-  	// }
+    Shipment_ID : { type: 'number' },
+    Shipment_Received : { type: 'time' },
+    Waybill_Number : { type: 'string' },
+    Shipment_Comments : { type: 'text' },
   },
 
   alias : {
-  	'id' : 'Shipment_ID',
+    'id' : 'Shipment_ID',
     'received' : 'Shipment_Received',
     'waybill_number' : 'Waybill_Number',
     'comments' : 'Shipment_Comments',
@@ -42,5 +30,6 @@ module.exports = {
     'Received_by' : 'FKRecipient_Employee__ID',
     'Sent_by' : 'FKSender_Employee__ID'
   }
-};
+ 
+});
 
