97c1e8396b30fe12feba9536badc18291a28ca65
Ran Guin <ran.guin@gmail.com>
2017-06-05 17:33:48 -0700

    standardize user methods

--- custom/core/models/User.js
+++ custom/core/models/User.js
@@ -11,7 +11,7 @@ var request = require('request');
 module.exports = {
 
   viewFields : ['id','name','createdAt'],
-
+  
   attributes: {
     // The user's full name
     // e.g. Nikola Tesla
@@ -19,6 +19,8 @@ module.exports = {
       type: 'string',
       required: true
     },
+
+    FK_Employee__ID : { type : 'int' }, // legacy reference to alDente ID .. 
 
     access: {
       type: 'string',
@@ -63,21 +65,6 @@ module.exports = {
       via: 'members'
     },
 
-    interests : {
-      collection: 'interest',
-      via: 'members'
-    },
-
-    activities : {
-      collection: 'activity',
-      via: 'members'
-    },
-
-    skills: {
-      collection: 'skill',
-      via: 'candidates'
-    },
-
     beforeCreate: function(values, next) {
         // ensure pwd matches confirmation pwd 
         if (!values.password || values.password != values.confirmation) {
@@ -97,12 +84,6 @@ module.exports = {
       return obj;
     }
   },
-
-  initData : [
-    { name : 'Admin', email : 'admin@domain.com' },
-    { name : 'DemoUser', email: 'demo@domain.com' },
-    { name : 'Guest', email : 'guest@domain.com' },
-  ],
 
   payload : function (user, options) {
       var deferred = q.defer();
@@ -147,6 +128,96 @@ module.exports = {
       deferred.resolve(payload);
       return deferred.promise;
   },
+
+  get_remote_session : function (session) {
+    var deferred = q.defer();
+
+    deferred.reject("No session specifications");
+    // replace with logic parsing external session from session parameters... should include security token of some sort for safety
+
+    return deferred.promise;
+
+  }
+
+  remote_session_payload : function (session) {
+    // eanble validation based upon a separate UI session (customized session retrieval using 'get_remote_session()' )
+    console.log("Validate: " + session);
+
+    var deferred = q.defer();
+
+    User.get_remote_session(session)
+    .then ( function (remote) {
+     // Try to look up user using the provided email address
+      User.findOne(remote.user, function foundUser(err, user) {
+
+        if (err) { deferred.reject(err) }
+
+        else if (!user || (user == 'undefined') ) { 
+          console.log("Unrecognized User: " + username + '/' + userid);
+          var err = new Error('unrecognized user: ' + username + '/' + userid);
+          deferred.reject(err);
+        }
+        else {
+          console.log("remote access granted");
+          User.payload(user)
+          .then (function (payload) {
+
+            payload['remote_login'] = remote.info;
+            // session authorization
+         
+            console.log("resolve remote payload: " + JSON.stringify(payload));
+            deferred.resolve(payload);
+          })
+          .catch ( function (err) {
+            console.log("Error generating payload");
+            deferred.reject(err);
+          });
+        }
+      });            
+    })
+    .catch ( function (err) {
+      var err = new Error('session not validated');
+      deferred.reject(err);      
+    })
+        
+    return deferred.promise;
+  }
+
+  validate : function  (tryuser) {
+    // customizable ... 
+    var deferred = q.defer();
+
+    // , user.FK_Employee__ID as alDenteID
+
+    var query = "SELECT user.id, user.name, encryptedPassword, email, user.access FROM user"
+    + " WHERE email ='" + tryuser + "' OR user.name = '" + tryuser + "'" 
+    + " GROUP BY user.id";
+
+    console.log("Q: " + query);
+    Record.query_promise(query)
+    .then (function (result) {
+      deferred.resolve(result);
+    })
+    .catch ( function (err) {
+      deferred.reject(err);
+    })
+    return deferred.promise;
+  },
+
+  // Note:  additional custom key values (besideds id, name, password, email & access) in query above should match custom key values provided below 
+
+  validate_registration : function (data) {
+
+    var deferred = q.defer();
+
+    deferred.resolve([{ }]);   // may be used to retrieve external id referenced via a different database or table 
+
+    // Note: ( key / value pair will be included in payload) ... 
+
+    return deferred.promise;
+
+  },
+
 
 };
 
