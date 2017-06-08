91360e07c89ddd1b6c466659c11407b7e0d502c6
Ran Guin <rguin@Rans-iMac.local>
2017-06-08 15:02:39 -0700

    separate addition of Prep records into Prep module

--- api/models/Prep.js
+++ api/models/Prep.js
@@ -4,6 +4,8 @@
 * @description :: TODO: You might write a short summary of how this model works and what it represents here.
 * @docs        :: http://sailsjs.org/#!documentation/models
 */
+var q = require('q');
+var _ = require('underscore-node');
 
 module.exports = {
 
@@ -25,7 +27,7 @@ module.exports = {
   alias : {
     'id' : 'Prep_ID',
     comments : 'Prep_Comments'
-  }
+  },
   /** NEW 
   attributes: {
   	name : { type : 'string' },
@@ -38,6 +40,43 @@ module.exports = {
     Lab_protocol : { model : 'lab_protocol' },
   },
   **/
+
+  save_Prep : function (data, plateData) {
+
+    var deferred = q.defer();
+
+    Record.createNew('prep', data)
+    .then (function (result) {
+        console.log("Added Prep(s): " + JSON.stringify(result));
+
+        var PrepResult = result;
+        var ids = [];
+        var prepId = PrepResult.insertId;  // Legacy
+        var added = PrepResult.affectedRows;
+
+
+        for (var i=0; i<plateData.length; i++) {
+          plateData[i]['FK_Prep__ID'] = prepId;
+        }
+        
+        Record.createNew('plate_prep', plateData )
+        .then (function (result2) {
+          console.log("Added Plate_Prep: " + JSON.stringify(result2[0] + '...'));
+          deferred.resolve({ Prep: result, Plate_Prep: result2})
+        })
+        .catch ( function (err2) {
+          console.log('Error creating container references to prep');
+          deferred.reject(err2);
+        }); 
+    })
+    .catch ( function (err) {
+      console.log("Error creating prep record");
+      deferred.rejecdt(err);
+    });
+
+    return deferred.promise;         
+  }
+      
 
 };
 
