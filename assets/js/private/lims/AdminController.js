2d94c028855100d98defa1597939c4424e65ab72
Ran Guin <rguin@Rans-iMac.local>
2017-06-13 11:40:03 -0700

    remove external dependencies

--- assets/js/private/lims/AdminController.js
+++ assets/js/private/lims/AdminController.js
@@ -13,28 +13,41 @@ function adminController ($scope, $rootScope, $http, $q ) {
 		$scope.initialize_payload(config);
 	}
 
-	$scope.activate_user = function () {
+	console.log("LOAD Admin Controller");
+
+
+	$scope.activate_user = function (user) {
 
 		var status = 'active';
 		var access = 'lab';
 
-		if ($scope.access == 'deactivate') { status = 'inactive' }
-		else if ($scope.access == 'lab admin') { access = 'lab admin'}
-		else if ($scope.access == 'admin' ) { access = 'admin' }
+		if (user.id && user.name) {  // defined by setup_Menu... 
 
-		var data = { user : { id: $scope.limsuser.id, access: access, status: status} };
+			if ($scope.access == 'deactivate') { status = 'inactive' }
+			else if ($scope.access == 'lab admin') { access = 'lab admin'}
+			else if ($scope.access == 'admin' ) { access = 'admin' }
 
-		console.log('Set access for ' + $scope.limsuser.id + ' to ' + status + ' ' + access + ' = ' + $scope.access);
+			var data = { user : { id: user.id, access: access, status: status} };
 
-		$http.post('/User/activate', data)
-		.then ( function (response) {
-			var msg = "Activated  " + $scope.limsuser.name + " with " + response.data.access + ' access';
-			console.log(msg);
-			$scope.message(msg);
-		})
-		.catch ( function (err) {
-			console.log("Error: " + err);
-			$scope.error("Error granting user access: " + err);
-		});
+			console.log('Set access for ' + user.name + ' [' + user.id + '] to ' + status + ' ' + access + ' = ' + $scope.access);
+
+			$http.post('/User/activate', data)
+			.then ( function (response) {
+				var msg = "Activated  " + user.name;
+				if (response.data && response.data.access) {
+					msg += " with " + response.data.access + ' access';
+				}
+				
+				console.log(msg);
+				$scope.message(msg);
+			})
+			.catch ( function (err) {
+				console.log("Error: " + err);
+				$scope.error("Error granting user access: " + err);
+			});
+		}
+		else {
+			$scope.error("User must first be selected");
+		}
 	}
 }]);
