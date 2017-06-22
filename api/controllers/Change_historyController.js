0e158332ad84262479fe645a4e49e9884513e7c0
Ran Guin <rguin@Rans-iMac.local>
2017-06-22 11:14:34 -0700

    update change history to use Change_history model & controllers

---
+++ api/controllers/Change_historyController.js
@@ -0,0 +1,11 @@
+/**
+ * Change_historyController
+ *
+ * @description :: Server-side logic for managing change_histories
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
--- api/models/Record.js
+++ api/models/Record.js
@@ -11,20 +11,6 @@ var _ = require('underscore-node');
 
 module.exports = {
 
-	migrate: 'safe',
-	tableName: 'Change_History',
-	primaryField: 'Change_History_ID',
-
-	attributes: {
-		FK_DBField__ID : { type : 'integer' },
-		Old_Value : { type : 'string'},
-		New_Value : { type : 'string'},
-		FK_Employee__ID : { type : 'integer'},
-		Modified_Date : { type : 'time' },
-		Record_ID : { type : 'int' },
-		Comment : { type : 'text'}
-	},
-
 	alias : function (model, alias, options) {
 		var Mod = sails.models[model] || {};
 
@@ -1327,7 +1313,7 @@ module.exports = {
 			}
 			console.log('update history .. ');
 
-			Record.update_History(model, ids, data, track, null, options.timestamp)
+			Change_history.update_History(model, ids, data, track, null, options.timestamp)
 			.then ( function (History) {
 				// console.log("History: " + JSON.stringify(History));
 				var promises = [];
@@ -1378,7 +1364,7 @@ module.exports = {
 						console.log(JSON.stringify(data));
 						console.log(JSON.stringify(History));
 
-						Record.update_History(model, ids, data, track, History, options.timestamp)
+						Change_history.update_History(model, ids, data, track, History, options.timestamp)
 						.then (function (finalHistory) {
 							console.log("Saved History after update...");
 							console.log(JSON.stringify(finalHistory));
@@ -1471,14 +1457,13 @@ module.exports = {
 			return deferred.promise;
 		}
 
-
 		var fields = [];
 		if (data.length) { fields = Object.keys(data[0]) }
 
 		var valid_fields = [];
 		fields.map( function (f) {
 			if (f && f !== '0') {
-				console.log('yes: ' + f);
+				// could potentially use this to validate actual field names - not just check for phantom 0 index... 
 				valid_fields.push(f);
 			}
 			else {
@@ -2256,196 +2241,6 @@ module.exports = {
         console.log(JSON.stringify(errors));
         if (! errors.length) { errors.push(err) }
         return errors;
-    },
-
-    update_History : function (model, ids, data, track, History, timestamp) {
-    	// retrieve values before record is updated  ... run in conjunction with similar call with History set.. 
-    	var deferred = q.defer();
-
-    	if (!timestamp) { timestamp = '<NOW>' }
-
-    	ids = Record.cast_to(ids, 'array');
-
-    	console.log('use model ' + model);
-    	var Mod = sails.models[model] || {};    	
-    	var	table = Mod.tableName || model;
-
-    	var key;
-    	track = track || ['FK_Rack__ID'];  // always track Rack_ID changes ... 
-
-    	console.log("\n*** update History with " + JSON.stringify(History));
-    	console.log(model);
-    	console.log(ids.join(','));
-    	console.log(track.length);
- 
-    	var save = false;
-
-    	if (model && ids && data && track && track.length && Mod) {
-	    	if (History) { 
-	    		key = 'New_Value'
-	    		save = true;
-	    	}
-	    	else { 
-	    		History = {};
-	    		key = 'Old_Value';
-	    	}
-
-	    	if (! History[table]) { History[table] = {} }
-
-    		var idField = Record.alias(model,'id');
-			
-			var query = "SELECT " + idField + ' as id, ' + track.join(',') + " FROM " + table + " WHERE " + idField + " IN (" + ids.join(',') + ')';
-
-			var Relocate = [];
-
-			console.log(query);
-			var promises = [];
-			promises.push( Record.query_promise(query) )
-			promises.push( Record.load_FK(table, track) );
-
-			q.all(promises)
-			.then (function (results) {
-				var result = results[0];
-				var FK = results[1];
-				var FKT = FK[table];
-
-				console.log("FK: " + JSON.stringify(FK));
-
-				console.log(JSON.stringify(result));
-				for (var i=0; i<result.length; i++) {
-					for (var j=0; j<track.length; j++) {
-						var f = track[j];
-						var id = result[i].id;
-						var fk = FKT[f];
-
-						if (! History[table][id] ) { History[table][id] = {} }
-						if (! History[table][id][f] ) { History[table][id][f] = {} }
-
-						if (key === 'New_Value' && History[table][id][f]['Old_Value'] === result[i][f]) {
-							console.log(f + ' value unchanged...');
-							delete History[table][id][f];
-						}
-						else {
-							History[table][id][f][key] = result[i][f];	
-
-							console.log("Tracked History: " + JSON.stringify(History[table][id][f]) );
-
-							if (key === 'New_Value') {
-								History[table][id][f]['Record_ID'] = id;
-								History[table][id][f]['FK_DBField__ID'] = fk;
-								History[table][id][f]['Modified_Date'] = timestamp;
-								
-								if (sails && sails.config && sails.config.payload) {
-									History[table][id][f]['FK_Employee__ID'] = sails.config.payload.alDenteID;
-								}
-
-								if (f === 'FK_Rack__ID') {
-									var relocate = {};
-									// relocate['class'] = table;
-									relocate['Container'] = id;
-									// relocate['field'] = f;
-									relocate['Moved_from'] = History[table][id][f]['Old_Value'];
-									relocate['Moved_to'] = result[i][f];
-									relocate['moved'] = timestamp;
-									relocate['Moved_by'] = '<userid>';
-									Relocate.push(relocate);
-								}
-
-							}
-						}
-					}
-				}
-
-				if (save) {
-					Record.saveHistory(History, Relocate)
-					.then ( function (response) {
-						console.log("Saved History: " + JSON.stringify(response));
-						deferred.resolve(History);
-					})
-					.catch ( function (err) {
-						console.log("Error saving History");
-						deferred.reject(err);
-					});
-				}
-				else { 
-					deferred.resolve(History);
-				}
-
-			})
-			.catch (function (err) {
-				console.log("Error saving preHistory: " + err);
-				deferred.reject(err);
-			});
-    	}
-    	else { 
-    		deferred.resolve();
-    	}
-
-    	return deferred.promise;
-    },
-
-    saveHistory : function (History, Relocate) {
-    	var deferred = q.defer();
-
-    	History = History || {};
-    	var Data = [];
-    	
-    	console.log("build data...");
-    	console.log("History: " + JSON.stringify(History));
-
-    	var tables = Object.keys(History);
-    	for (var i=0; i<tables.length; i++) {
-    		var ids = Object.keys(History[tables[i]]);
-    		for (var j=0; j<ids.length; j++) {
-    			var update = History[tables[i]][ids[j]];
-    			var fields = Object.keys(update);
-    			for (var k=0; k<fields.length; k++) {
-    				var data = update[fields[k]];
-	    			// data.FK_Employee__ID = sails.config.payload.alDenteID;
-    				// data.FK_DBField__ID  = FK[tables[i]][fields[k]];
-    				// data.Modified_Date   = 'NOW()';
-    				if (data.FK_DBField__ID) {
-    					Data.push( data );
-    				}
-    				else {
-    					console.log("Skipping " + tables[i] + '.' + fields[k] + ' (no DBField record found)');
-    				}
-    			}
-    		}
-    	}
-    	console.log("History Data: " + JSON.stringify(Data));
-    	console.log("Relocate: " + JSON.stringify(Relocate));
-    	deferred.resolve(Data);
-    	
-    	Record.createNew('Change_History', Data)
-    	.then ( function (result) {
-    		console.log("tracked Change History");
-	    	console.log("Relocate: " + JSON.stringify(Relocate));
-	    	Record.createNew('sample_tracking', Relocate)
-	    	.then ( function (relocated) {
-	    		deferred.resolve(relocated);
-	    	})
-	 		.catch ( function (err) {
-	 			console.log("Error tracking sample history: " + err);
-	 			deferred.reject(err);
-	 		});   	    		
-    	})
-    	.catch ( function (err) {
-    		console.log("Error tracking Change History");
-	    	
-	    	// perform sample tracking regardless ... if possible... 
-	    	Record.createNew('sample_tracking', Relocate)
-	    	.then ( function (relocated) {
-	    		console.log("Tracked sample movement (update Change History)");
-	    		deferred.resolve(relocated);
-	    	})
-	 		.catch ( function (err) {
-	 			console.log("Error tracking sample history as well: " + err);
-	 			deferred.reject(err);
-	 		});       		
-    	})
-    	
-    	return deferred.promise;
     },
 
     load_FK : function (table, fields) {
