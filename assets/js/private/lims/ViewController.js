55c94d9395322b522207d1f37ebd1abf2eb3c0c1
Ran Guin <ran.guin@gmail.com>
2017-09-09 23:08:55 -0700

    update view controller

--- assets/js/private/lims/ViewController.js
+++ assets/js/private/lims/ViewController.js
@@ -23,29 +23,83 @@ app.controller('ViewController',
 		$scope.id = config['id'];
 		$scope.file = config['file'];
 		$scope.excel = config['excel'];
+
+		var fields = $scope.view.fields;
+		var group  = {};
+		var search = {};
+		var show = {};
+		var layer = {};
+
+		$scope.form = {
+			view_id: $scope.id,
+			show: show,
+			fields: fields,
+			groupBy:  group,
+			search: search,
+			layer: layer
+		}
+
+		$scope.form.show['Plate_ID'] = true;
+		$scope.form.show['FK_Library__Name'] = true;
+		$scope.show = ['Plate_ID', 'FK_Library__Name'];
+
+		$scope.form.groupBy['FK_Library__Name'] = true;
+		$scope.groupBy = ['FK_Library__Name'];
+
+		$scope.form.layer['FK_Library__Name'] = true;
+		$scope.layer = ['FK_Library__Name'];
+
 	}
 
 	$scope.generate = function () {
 		var url = "/view";
-
 		var options = $scope.view || {};
-		
-		options.id = $scope.id;
-		options.generate = true;
-		options.fields = ['Employee_Name', 'thaws'];
 
-		console.log("POST: " + url);
-		console.log(JSON.stringify(options));
+		var data = {
+			view : $scope.view,
+			fields : $scope.show,
+			group  : $scope.groupBy,
+			layer  : $scope.layer,
+			conditions : $scope.conditions
+		}		
+		console.log("FORM: " + JSON.stringify($scope.form));
 
-		$http.post(url, options)
-		.then ( function (result) {
-			console.log("requested data generation for this view");
-			console.log(JSON.stringify(result));
-			$scope.message("Generated Results");
-		})
-		.catch ( function (err) {
-			console.log("Error generating view");
-		});
+		if (0) {
+			options.id = $scope.id;
+			options.generate = true;
+			options.fields = ['Employee_Name', 'thaws'];
+
+			console.log("POST: " + url);
+			console.log(JSON.stringify(options));
+
+			$http.post(url, options)
+			.then ( function (result) {
+				console.log("requested data generation for this view");
+				console.log(JSON.stringify(result));
+				$scope.message("Generated Results");
+			})
+			.catch ( function (err) {
+				console.log("Error generating view");
+			});
+		}
+	},
+
+	$scope.updateList = function (list, item) {
+		console.log("update " + list + ' with ' + item);
+
+		console.log('L1: ' + JSON.stringify($scope[list]));
+		if ($scope.form[list][item]) {
+			console.log('add ' + item);
+			$scope[list].push(item);
+		}
+		else {
+			console.log('remove ' + item);
+			var index = $scope[list].indexOf(item);
+			if (index >= 0) {
+				$scope[list].splice(index, 1);
+			}
+			
+		}
 	}
 
 	$scope.regenerate = function () {
