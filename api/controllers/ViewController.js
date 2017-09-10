2b61501902706303d4b72697dd77863cbf215580
Ran Guin <ran.guin@gmail.com>
2017-09-10 03:05:52 -0700

    update view generation

--- api/controllers/ViewController.js
+++ api/controllers/ViewController.js
@@ -5,6 +5,8 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+var excel_path = "./data/excel/";
+
 module.exports = {
 
 	download: function (req, res) {
@@ -14,7 +16,7 @@ module.exports = {
 		console.log("download: " + file);
 		if (file) {
 			console.log("download " + file);
-			return res.download(file);
+			return res.download(excel_path + file);
 		}
 		else {
 			return res.send("file not found");
@@ -25,117 +27,120 @@ module.exports = {
 		console.log('build view...');
 		var body = req.body || {};
 		var view_id = body.view_id  || body.id || req.param('id');
-		var generate = body.generate || req.param('generate');
+
 		var fields = body.fields || req.param('fields');
-		var table = body.table || req.param('table');
-		var save = body.save || req.param('save') || 1;
-		var page = body.page || req.param('page') || 1;
-		var page_size = body.page_size || req.param('page_size') || 50;
+		var layer  = body.layer || req.param('layer');
+		var conditions = body.conditions || req.param('conditions');
 
 		if (fields && fields.constructor === String) {
 			fields = fields.split(/,\s*/);
 		}
 
-		if (!view_id) {
-			console.log("no view specified...");
-			View.specs()
-			.then ( function (result) {
-				console.log("R: " + JSON.stringify(result));
-				return res.render('lims/Views', { 
-					views : result, 
-					title: 'Views',
-					fields: ['name', 'tables', 'fields'] 
-				});
-			})
-			.catch ( function (err) {
-				console.log("error retrieving views:");
-				console.log(err);
-				res.render('core/500.jade')
-			})
+		var options = {
+			fields: fields,
+			layer: layer,
+			conditions: conditions
+		};
+
+		View.setup(view_id, options)
+		.then ( function (setup) {
+			console.log("options: " + JSON.stringify(options));
+			console.log("baseline query: " + setup.query);
+			console.log("setup: " + JSON.stringify(setup));
+
+			setup.message = 'view initiated';
+			return res.render('lims/View', setup);
+		})
+		.catch ( function (err) {
+			console.log("error setting up intitial view");
+			options.message = 'view initiated';
+
+			return res.render('lims/View', options);			
+		});
+	},
+
+	generate: function (req, res) {
+
+		console.log('build view...');
+		var body = req.body || {};
+
+		var view_id = body.view_id  || body.id || req.param('id');
+
+		var fields = body.fields || req.param('fields');
+		var layer  = body.layer || req.param('layer');
+		var conditions = body.conditions || req.param('conditions');
+		var limit = body.limit;
+
+		var page = body.page || req.param('page') || 1;
+		var page_size = body.page_size || req.param('page_size') || 50;
+
+		var save = body.save || req.param('save') || 1;
+		var filename = body.filename;
+
+		if (fields && fields.constructor === String) {
+			fields = fields.split(/,\s*/);
 		}
-		else {
-			// view_id is defined .... 
-			console.log(view_id + " VIEW: " + JSON.stringify(fields));
 
-			View.specs(view_id)
-			.then ( function (specs) {
-				if (!specs) { specs = [] }
-				View.build(view_id, {fields: fields, generate: generate})
-				.then ( function (result) {
-					console.log("RESULTS: " + JSON.stringify(result));
+		var options = {
+			fields: fields,
+			layer: layer,
+			conditions: conditions,
+			limit: limit
+		};
 
-					if (generate) {
-						console.log("generated results...");
-						if (save && result.data) {
-							console.log('save as excel');
+		View.generate(view_id, options)
+		.then (function (result) {
+			console.log('generated view');
+			if (save && result.data) {
+				console.log('save as excel');
 
-							View.save2excel(result.data)
-							.then ( function (excel) {
-								console.log("saved as excel");
-								console.log(JSON.stringify(excel.stats))
-								// res.send({data: result.data});
-								
-								// paginate results 
-								return res.render('lims/View', {
-									excel: excel,
-									view : specs[0],
-									data : result.data,
-									query: result.query,
-									id: view_id,
-									message: 'Excel file saved:  ',
-									records: result.data.length,
-									page: page
-								});
-							})
-							.catch ( function (err) {
-								console.log("Error saving as excel...");
-								// res.send({data: result.data});
-								return res.render('lims/View', { 
-									view : specs[0],
-									data : result.data,
-									query: result.query,
-									id: view_id,
-									message: 'error saving'
-								});
-								return res.send("error saving as excel");
-							});
-						}
-						else {
-							console.log('nothing saved ... ');
-							console.log('query: ' + result.query);
-							// res.send({data: result.data});
-							return res.render('lims/View', { 
-								data: result.data,
-								query: result.query,
-								view : specs[0],
-								id: view_id,
-								message: 'nothing saved'
-							});								
-						}
-					}
-					else {
-						console.log("baseline query: " + result.query);
-						return res.render('lims/View', { 
-							view : specs[0],
-							id: view_id,
-							message: 'nothing generated'
-						});
-					}
+				View.save2excel(result.data, {path: excel_path, filename: filename})
+				.then ( function (excel) {
+					console.log("saved as excel");
+					return res.json({data: result.data, message: 'Excel file saved', excel: excel });
+
+					// paginate results 
+					// return res.render('lims/View', {
+					// 	excel: excel,
+					// 	view : result.view,
+					// 	data : result.data,
+					// 	query: result.query,
+					// 	id: view_id,
+					// 	message: 'Excel file saved:  ',
+					// 	records: result.data.length,
+					// 	page: page
+					// });
 				})
 				.catch ( function (err) {
-					res.send(err);
-				})
-			})
-			.catch ( function (err) {
-				res.send(err);
-			});
-		}
+					console.log("Error saving as excel...");
+					return res.json({data: result.data});
+				// return res.render('lims/View', { 
+					// 	view : result.view,
+					// 	data : result.data,
+					// 	query: result.query,
+					// 	id: view_id,
+					// 	message: 'error saving'
+					// });
+				});
+			} else {
+				console.log('nothing saved ... ');
+				options.message = 'nothing saved';
+				return res.json({});
+				// return res.render('lims/View', options);								
+			}
+		})
+		.catch (function (err) {
+			console.log("Error generating view");
+			options.message = 'error generating view';
+			return res.json({});
+			// return res.render('lims/View', options);
+		});
 	},
 
 	views: function (req, res) {
 		console.log('show views...');
 
-		View.specs()
+		View.list()
 		.then ( function (result) {
 			console.log("R: " + JSON.stringify(result));
 			return res.render('lims/Views', { 
