77790f2e2c663f9de768a71f5856308515a7002f
Ran Guin <ran.guin@gmail.com>
2017-10-13 14:52:13 -0700

    reload renamed files

---
+++ api/models/Original_source.js
@@ -0,0 +1,23 @@
+/**
+* Original_Source.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  migrate: 'safe',
+  tableName: 'Original_Source',
+
+  attributes: {
+  	Original_Source_ID: { type: 'integer'},
+    Original_Source_Name: { type: 'string'},
+  	FK_Patient__ID: { model: 'patient'},
+  },
+
+  alias : {
+  	id: 'Original_Source_ID'
+  }
+};
+
